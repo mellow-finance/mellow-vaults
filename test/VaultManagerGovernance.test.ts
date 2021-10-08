52a38d966ae4e465ea2e087685d808511896c942
Mikhail S <mihanix.it@gmail.com>
2021-10-08 15:25:27 +0300

    add tests for VaultManagerGovernance co-authored by @grigorevPavel

--- test/VaultManagerGovernance.test.ts
+++ test/VaultManagerGovernance.test.ts
@@ -3,25 +3,65 @@ import { ethers } from "hardhat";
 import type * as ethersT from "ethers";
 
 
-describe("VaultManagerGovernance tests", function() {
+describe("VaultManagerGovernance", function() {
+    let ProtocolGovernance: ethersT.ContractFactory;
     let vaultManagerGovernance: ethersT.Contract;
     let protocolGovernance: ethersT.Contract;
     let deployer: ethersT.Signer;
     let stranger: ethersT.Signer;
 
-
     beforeEach(async function() {
         const Common = await ethers.getContractFactory("Common");
         await Common.deploy();
         const VaultManagerGovernance = await ethers.getContractFactory("VaultManagerGovernance");
-        const ProtocolGovernance = await ethers.getContractFactory("ProtocolGovernance");
+        ProtocolGovernance = await ethers.getContractFactory("ProtocolGovernance");
         [deployer, stranger] = await ethers.getSigners();
 
         protocolGovernance = await ProtocolGovernance.deploy();
         vaultManagerGovernance = await VaultManagerGovernance.deploy(true, protocolGovernance.address);
     });
 
-    it("Governance params should be set", async function() {
-        expect(await vaultManagerGovernance.governanceParams()).to.deep.equal([true, protocolGovernance.address]);
+    it("Governance params should be set", async () => {
+        expect(await vaultManagerGovernance.governanceParams()).to.deep.equal(
+            [true, protocolGovernance.address]
+        );
+    });
+
+    describe("Set pending governance params test", () => {
+        let newProtocolGovernance: any;
+    
+        beforeEach(async () => {
+            newProtocolGovernance = await ProtocolGovernance.deploy();
+        });
+    
+        it("Role should be governance or delegate", async () => {
+            await expect(
+                vaultManagerGovernance.connect(stranger).setPendingGovernanceParams([false, newProtocolGovernance.address])
+            ).to.be.revertedWith("GD");
+        });
+
+        it("Pending governance params address should not be equal to 0x0", async () => {
+            let zeroAddress = ethers.constants.AddressZero;
+            await expect(
+                vaultManagerGovernance.setPendingGovernanceParams([false, zeroAddress])
+            ).to.be.revertedWith("ZMG");
+        });
+
+        it("Pending governance timestamp should be set", async() => {
+            /// todo: manage timestamp 
+        });
+
+        it("Should emit new event SetPendingGovernanceParams", async () => {
+            await expect(
+                vaultManagerGovernance.setPendingGovernanceParams([false, newProtocolGovernance.address])
+            ).to.emit(vaultManagerGovernance, "SetPendingGovernanceParams").withArgs([false, newProtocolGovernance.address]);
+        })
+
+        it("Pending governance params should be set", async () => {
+            await vaultManagerGovernance.setPendingGovernanceParams([false, newProtocolGovernance.address]);
+            expect(
+                await vaultManagerGovernance.pendingGovernanceParams()
+            ).to.deep.equal([false, newProtocolGovernance.address]);
+        });
     });
 });
