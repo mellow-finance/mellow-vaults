59b4f47a7320b285bd280aebf5c0bc54bd60b479
Mikhail S <mihanix.it@gmail.com>
2021-10-11 16:33:22 +0300

    maybe fix ganache ci

--- contracts/test/ERC20Test.sol
+++ contracts/test/ERC20Test.sol
@@ -1,9 +1,9 @@
-// SPDX-Licence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ERC20Mock is ERC20 {
+contract ERC20Test is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
         _mint(msg.sender, 10**18);
     }
