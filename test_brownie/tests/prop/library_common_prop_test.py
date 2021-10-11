a42eae01b819a8ebf620c580abd6ef348eed1ead
Mikhail S <mihanix.it@gmail.com>
2021-10-11 17:21:41 +0300

    use typing style before python3.9

--- test_brownie/tests/prop/library_common_prop_test.py
+++ test_brownie/tests/prop/library_common_prop_test.py
@@ -1,23 +1,24 @@
-# from brownie import accounts
-# from brownie.test import given, strategy
-# from brownie.typing import AccountsType
+from typing import List
+
+from brownie import accounts
+from brownie.test import given, strategy
+from brownie.typing import AccountsType
 
 
-# def _addresses_to_int(addresses: list[AccountsType]) -> list[int]:
-#     return [(address.address.lower(), 16) for address in addresses]
+def _addresses_to_int(addresses: List[AccountsType]) -> List[int]:
+    return [(address.address.lower(), 16) for address in addresses]
 
 
-# def _is_unique(seq: list) -> bool:
-#     return len(seq) == len(set(seq))
+def _is_unique(seq: list) -> bool:
+    return len(seq) == len(set(seq))
 
 
-# @given(addresses=strategy("address[]", min_length=0, max_length=15, unique=False))
-# def test_sorted_and_unique(addresses, a, CommonTest):
-#     common_test = CommonTest.deploy({"from": a[0]})
-#     print(common_test.isSortedAndUnique(addresses))
-#     int_addresses = _addresses_to_int(addresses)
-#     int_addresses_sorted = [_ for _ in sorted(int_addresses)]
-#     assert common_test.isSortedAndUnique(addresses) == (
-#         int_addresses == int_addresses_sorted and
-#         _is_unique(int_addresses)
-#     )
+@given(addresses=strategy("address[]", min_length=0, max_length=15, unique=False))
+def test_sorted_and_unique(addresses, a, CommonTest):
+    common_test = CommonTest.deploy({"from": a[0]})
+    int_addresses = _addresses_to_int(addresses)
+    int_addresses_sorted = [_ for _ in sorted(int_addresses)]
+    assert common_test.isSortedAndUnique(addresses) == (
+        int_addresses == int_addresses_sorted and
+        _is_unique(int_addresses)
+    )
