99060fe2d81f81f3800031c4d0ab9a494a808a0c
AlexK <ak@mellow.finance>
2021-10-24 12:35:57 +0300

    added interface docs

--- contracts/interfaces/IVaultGovernanceFactory.sol
+++ contracts/interfaces/IVaultGovernanceFactory.sol
@@ -5,6 +5,11 @@ import "./IVaultGovernance.sol";
 import "./IVaultManager.sol";
 
 interface IVaultGovernanceFactory {
+    /// @notice Deploy new vault governance
+    /// @param tokens A set of tokens that will be managed by the Vault
+    /// @param manager Reference to Vault Manager
+    /// @param treasury Strategy treasury address that will be used to collect Strategy Performance Fee
+    /// @param admin Admin of the Vault
     function deployVaultGovernance(
         address[] memory tokens,
         IVaultManager manager,
--- contracts/interfaces/IVaultManagerGovernance.sol
+++ contracts/interfaces/IVaultManagerGovernance.sol
@@ -6,6 +6,11 @@ import "./IVaultFactory.sol";
 import "./IVaultGovernanceFactory.sol";
 
 interface IVaultManagerGovernance {
+    /// @notice Governance params of the Vault Manager
+    /// @param permissionless Anyone can create a new vault
+    /// @param protocolGovernance Governance of the protocol
+    /// @param factory Vault Factory reference
+    /// @param governanceFactory VaultGovernance Factory reference
     struct GovernanceParams {
         bool permissionless;
         IProtocolGovernance protocolGovernance;
@@ -13,14 +18,19 @@ interface IVaultManagerGovernance {
         IVaultGovernanceFactory governanceFactory;
     }
 
+    /// @notice Current governance params
     function governanceParams() external view returns (GovernanceParams memory);
 
+    /// @notice Staged governance params
     function pendingGovernanceParams() external view returns (GovernanceParams memory);
 
+    /// @notice When staged governance params can be committed
     function pendingGovernanceParamsTimestamp() external view returns (uint256);
 
+    /// @notice Set staged governance params
     function setPendingGovernanceParams(GovernanceParams calldata newParams) external;
 
+    /// @notice Commit staged governance params
     function commitGovernanceParams() external;
 
     event SetPendingGovernanceParams(GovernanceParams);
