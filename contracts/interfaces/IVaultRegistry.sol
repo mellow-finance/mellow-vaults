d491d5d10a7f1650531a76de683a6ea9db66c365
Mikhail S <mihanix.it@gmail.com>
2021-10-26 17:06:04 +0300

    update VaultRegistry interface

--- contracts/interfaces/IVaultRegistry.sol
+++ contracts/interfaces/IVaultRegistry.sol
@@ -3,15 +3,29 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./IVault.sol";
+import "./IVaultFactory.sol";
+import "./IProtocolGovernance.sol";
 
 interface IVaultRegistry is IERC721 {
-    /// @notice Register new Vault Group and mint NFT
-    /// @param vault Address of the Vault contract
-    /// @return nftId of the NFT minted for the given Vault Group
-    function registerVault(IVault vault)
-        external
-        returns
-        (uint256 nftId);
+    function registerVaultKind (
+        IVaultFactory vaultFactory,
+        IVaultGovernance vaultGovernance
+    ) external returns (uint256);
+
+    function vaultGovernanceForNft(uint256 nftId) external view returns (IVaultGovernance);
+    function vaultFactoryForNft(uint256 nftId) external view returns (IVaultFactory);
+
+    function vaultGovernanceForVault(IVault vault) external view returns (IVaultGovernance);
+    function vaultFactoryForVault(IVault vault) external view returns (IVaultFactory);
+
+    /// @notice Register new Vault and mint NFT
+    /// @param vaultKind ID of the vault kind
+    /// @return vault Address of created Vault
+    /// @return nftId ID the NFT minted for the given Vault Group
+    function createVault(uint256 vaultKind) external returns (
+        IVault vault,
+        uint256 nftId
+    );
     
     /// @notice Get Vault for the giver NFT ID
     /// @param nftId NFT ID
