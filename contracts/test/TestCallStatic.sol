b1c9ae64df5088484ee47a504a43942d9687f920
Mikhail S <mihanix.it@gmail.com>
2021-10-26 15:33:23 +0300

    add test call static

---
+++ contracts/test/TestCallStatic.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract TestCallStatic {
+  uint256 private _a;
+
+  function incA() external returns (uint256) {
+    _a += 1;
+    return _a;
+  }
+
+  function a() external view returns (uint256) {
+    return _a;
+  }
+}
