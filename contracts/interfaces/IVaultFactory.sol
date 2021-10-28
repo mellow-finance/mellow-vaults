dce29265fc68766a6174848be6335854a348094a
Mikhail S <mihanix.it@gmail.com>
2021-10-28 17:17:00 +0300

    remove VaultGovernanceFactory & VaultGovernacneOld & rename VaultFactoryV2

--- contracts/interfaces/IVaultFactory.sol
+++ contracts/interfaces/IVaultFactory.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "./IVaultGovernanceOld.sol";
+import "./IVaultGovernance.sol";
 import "./IVault.sol";
 
-interface IVaultFactory {
+interface IVaultFactoryV2 {
     /// @notice Deploy a new vault
     /// @param vaultGovernance Reference to Vault Governance
     /// @param options Deployment options (varies between vault factories)
-    function deployVault(IVaultGovernanceOld vaultGovernance, bytes calldata options) external returns (IVault vault);
+    function deployVault(IVaultGovernance vaultGovernance, bytes calldata options) external returns (IVault vault);
 }
