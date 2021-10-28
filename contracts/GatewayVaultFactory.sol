e36cd0002f2a155e07c5ecd316206c83e132f446
Mikhail S <mihanix.it@gmail.com>
2021-10-28 17:16:27 +0300

    fix GatewayVaultFactory

--- contracts/GatewayVaultFactory.sol
+++ contracts/GatewayVaultFactory.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "./GatewayVaultManager.sol";
+import "./interfaces/IVaultFactory.sol";
 import "./GatewayVault.sol";
 
 contract GatewayVaultFactory {
     function deployVault(
         IVaultGovernance vaultGovernance,
-        address[] memory tokens,
-        address[] memory vaults
+        bytes calldata options
     ) external returns (IVault) {
+        address[] memory tokens;
+        address[] memory vaults;
+        (tokens, vaults) = abi.decode(options, (address[], address[]));
         GatewayVault gatewayVault = new GatewayVault(vaultGovernance, tokens, vaults);
         return IVault(gatewayVault);
     }
