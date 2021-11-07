c4c74d13557629f671097b268c143192c514150b
Mikhail S <mihanix.it@gmail.com>
2021-11-07 04:32:49 +0300

    Update Exceptions.ts

--- test/library/Exceptions.ts
+++ test/library/Exceptions.ts
@@ -14,7 +14,7 @@ export default class Exceptions {
         "ERC20: transfer amount exceeds balance";
     static readonly VALID_PULL_DESTINATION: string = "INTRA";
     static readonly CONTRACT_REQUIRED: string = "C";
-    static readonly SHOULD_BE_CALLED_BY_VAULT_GOVERNANCE = "AVG";
+    static readonly SHOULD_BE_CALLED_BY_VAULT_GOVERNANCE = "VG";
     static readonly REQUIRE_AT_LEAST_ADMIN: string = "RST";
 }
 // TODO: Remove outdated exceptions
