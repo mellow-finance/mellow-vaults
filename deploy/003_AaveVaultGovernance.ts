c5176c1b3103daf0aff07f3ca2740719e24eeb40
AlexK <ak@mellow.finance>
2021-11-08 20:42:14 +0300

    added aave vault factory init

--- deploy/003_AaveVaultGovernance.ts
+++ deploy/003_AaveVaultGovernance.ts
@@ -1,10 +1,11 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre;
-    const { deploy, get } = deployments;
+    const { deploy, get, log } = deployments;
     const protocolGovernance = await get("ProtocolGovernance");
     const vaultRegistry = await get("VaultRegistry");
     const { deployer, aaveLendingPool } = await getNamedAccounts();
@@ -20,15 +21,21 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         log: true,
         autoMine: true,
     });
-    const governance = await get("AaveVaultGovernance");
+    const governance = await hre.ethers.getContract("AaveVaultGovernance");
     await deploy("AaveVaultFactory", {
         from: deployer,
         args: [governance.address],
         log: true,
         autoMine: true,
     });
-    const factory = await get("AaveVaultFactory");
-    // await factory. initialize()
+    const initialized = await governance.initialized();
+    if (!initialized) {
+        log("Initializing factory...");
+
+        const factory = await get("AaveVaultFactory");
+        const receipt = await governance.initialize(factory.address);
+        log(`Initialized with txHash ${receipt.hash}`);
+    }
 };
 export default func;
 func.tags = ["ProtocolGovernance", "Vaults"];
