ebfa02e51351d13e6f6d4ae18d2b8e6303b1a54e
Mikhail S <mihanix.it@gmail.com>
2021-11-10 21:02:48 +0300

    added some baseline tests for gateway vault

--- test/GatewayVault.test.ts
+++ test/GatewayVault.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { ethers, deployments } from "hardhat";
-import { Signer } from "ethers";
+import { BigNumber, Signer } from "ethers";
 import {
     ERC20,
     Vault,
@@ -18,9 +18,12 @@ describe("GatewayVault", () => {
     let strategy: Signer;
     let anotherTreasury: Signer;
     let vaultGovernance: VaultGovernance;
+    let anotherVaultGovernance: VaultGovernance;
     let protocolGovernance: ProtocolGovernance;
     let vault: Vault;
+    let anotherVault: Vault;
     let nft: number;
+    let anotherNft: number;
     let tokens: ERC20[];
     let gatewayVault: Vault;
     let gatewayVaultGovernance: VaultGovernance;
@@ -32,13 +35,32 @@ describe("GatewayVault", () => {
             await ethers.getSigners();
         deployment = deployments.createFixture(async () => {
             await deployments.fixture();
-            ({ gatewayVault } = await deploySubVaultXGatewayVaultSystem({
+            ({
+                gatewayVault,
+                gatewayVaultGovernance,
+                vault,
+                vaultGovernance,
+                protocolGovernance,
+                nft,
+                anotherNft,
+                tokens,
+                anotherVault,
+                anotherVaultGovernance,
+            } = await deploySubVaultXGatewayVaultSystem({
                 adminSigner: admin,
                 treasury: await treasury.getAddress(),
                 vaultOwnerSigner: deployer,
                 strategy: await strategy.getAddress(),
                 vaultType: "ERC20Vault",
             }));
+            for (let i: number = 0; i < tokens.length; ++i) {
+                await tokens[i].connect(deployer).approve(
+                    gatewayVault.address,
+                    BigNumber.from(10 ** 9)
+                        .mul(BigNumber.from(10 ** 9))
+                        .mul(BigNumber.from(10 ** 9))
+                );
+            }
         });
     });
 
@@ -46,11 +68,90 @@ describe("GatewayVault", () => {
         await deployment();
     });
 
+    describe("vaultTokens", () => {
+        it("returns correct vault tokens", async () => {
+            expect(await vault.vaultTokens()).to.deep.equal(
+                tokens.map((token) => token.address)
+            );
+        });
+    });
+
+    describe("push", () => {
+        // address
+    });
+
     describe("constructor", () => {
         it("creates GatewayVault", async () => {
             expect(
                 await deployer.provider?.getCode(gatewayVault.address)
             ).not.to.be.equal("0x");
+        });
+    });
+
+    describe("subvaultNfts", () => {
+        it("returns nfts", async () => {
+            expect(await gatewayVault.subvaultNfts()).to.be.deep.equal([
+                nft,
+                anotherNft,
+            ]);
+        });
+    });
+
+    describe("tvl", () => {});
+
+    describe("earnings", () => {
+        describe("when nothing pushed yet", () => {
+            it("returns empty earnings", async () => {
+                expect(await gatewayVault.earnings()).to.deep.equal([]);
+            });
+        });
+    });
+
+    describe("subvaultTvl", () => {
+        describe("when nothing pushed yet", () => {
+            it("returns empty tvl", async () => {
+                expect(await gatewayVault.subvaultTvl(0)).to.deep.equal([]);
+            });
+        });
+    });
+
+    describe("subvaultsTvl", () => {
+        describe("when nothing pushed yet", () => {
+            it("returns empty tvl", async () => {
+                expect(await gatewayVault.subvaultsTvl()).to.be.deep.equal([
+                    [],
+                    [],
+                ]);
+            });
+        });
+    });
+
+    describe("vaultEarnings", () => {
+        describe("when nothing pushed yet", () => {
+            it("returns empty earnings", async () => {
+                expect(await gatewayVault.vaultEarnings(0)).to.be.deep.equal(
+                    []
+                );
+            });
+        });
+    });
+
+    describe("hasSubvault", () => {
+        describe("when passed actual vault", () => {
+            it("returns true", async () => {
+                expect(await gatewayVault.hasSubvault(vault.address)).to.be
+                    .true;
+                expect(await gatewayVault.hasSubvault(anotherVault.address)).to
+                    .be.true;
+            });
+        });
+
+        describe("when passed not vault", () => {
+            it("returns false", async () => {
+                expect(
+                    await gatewayVault.hasSubvault(await stranger.getAddress())
+                ).to.be.false;
+            });
         });
     });
 });
