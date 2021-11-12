b1b8784a72b8a28f7527f45e9e174368280cd33b
Mikhail S <mihanix.it@gmail.com>
2021-11-12 08:19:52 +0300

    test transferAndPush returns leftovers

--- test/GatewayVault.test.ts
+++ test/GatewayVault.test.ts
@@ -146,6 +146,31 @@ describe("GatewayVault", () => {
             });
         });
 
+        describe("when leftovers happen", () => {
+            it("returns them!", async () => {
+                const amount = BigNumber.from(10 ** 9);
+                expect(BigNumber.from(await tokens[0].balanceOf(await deployer.getAddress())).mod(2)).to.equal(
+                    0
+                );
+                await tokens[0]
+                    .connect(deployer)
+                    .approve(gatewayVault.address, BigNumber.from(amount.mul(2).add(1)));
+                await expect(
+                    gatewayVault
+                        .connect(deployer)
+                        .transferAndPush(await deployer.getAddress(), [tokens[0].address], [amount], [])
+                ).to.emit(gatewayVault, "Push");
+                await expect(
+                    gatewayVault
+                        .connect(deployer)
+                        .transferAndPush(await deployer.getAddress(), [tokens[0].address], [amount.add(1)], [])
+                ).to.emit(gatewayVault, "Push");
+                expect(BigNumber.from(await tokens[0].balanceOf(await deployer.getAddress())).mod(2)).to.equal(
+                    0
+                );
+            });
+        });
+
         it("emits Push", async () => {
             await tokens[0]
                 .connect(deployer)
