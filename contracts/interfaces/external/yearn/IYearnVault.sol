b08f236f1d1d3cd18efae4dd2a7aca78d533d7bf
AlexK <ak@mellow.finance>
2021-11-15 16:20:58 +0300

    added VaultGovernance

---
+++ contracts/interfaces/external/yearn/IYearnVault.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IYearnVault is IERC20 {
+    function deposit(uint256 amount, address recipient) external returns (uint256);
+
+    // Default maxLoss = 1, i.e. 0.01% [BPS]
+    function withdraw(
+        uint256 maxShares,
+        address recipient,
+        uint256 maxLoss
+    ) external returns (uint256);
+}
