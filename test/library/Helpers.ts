dc13f6d42a46f0753c46527e0e953c19e6acfb8f
Mikhail S <mihanix.it@gmail.com>
2021-11-18 14:22:39 +0300

    maybe fix ci

--- test/library/Helpers.ts
+++ test/library/Helpers.ts
@@ -1,8 +1,7 @@
 import { Contract } from "ethers";
-import { network } from "hardhat";
+import { network, ethers } from "hardhat";
 import { BigNumber } from "@ethersproject/bignumber";
 import { filter, fromPairs, keys, KeyValuePair, map, pipe } from "ramda";
-import { utils } from "ethers";
 
 export const toObject = (obj: any) =>
     pipe(
@@ -41,11 +40,11 @@ export const encodeToBytes = (
     types: string[],
     objectToEncode: readonly any[]
 ) => {
-    let toBytes = new utils.AbiCoder();
+    let toBytes = new ethers.utils.AbiCoder();
     return toBytes.encode(types, objectToEncode);
 };
 
 export const decodeFromBytes = (types: string[], bytesToDecode: string) => {
-    let fromBytes = new utils.AbiCoder();
+    let fromBytes = new ethers.utils.AbiCoder();
     return fromBytes.decode(types, bytesToDecode);
 };
