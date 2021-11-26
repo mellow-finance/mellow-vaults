685235eb9b1f67c9d4eb388d8fd3abe53ab5e44c
Mikhail S <mihanix.it@gmail.com>
2021-11-26 06:05:47 +0300

    add master trader interface

--- contracts/trader/IMasterTrader.sol
+++ contracts/trader/IMasterTrader.sol
@@ -1,6 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "../interfaces/IProtocolGovernance.sol";
+
 interface IMasterTrader {
-    // TODO: define
+    /// @notice Swap type
+    enum SwapType {
+        EXACT_INPUT_SINGLE,
+        EXACT_OUTPUT_SINGLE,
+        EXACT_INPUT_MULTIHOP,
+        EXACT_OUTPUT_MULTIHOP
+    }
+
+    /// @return the address of the protocol governance contract
+    function protocolGovernance() external view returns (address);
+
+    /// @param traderAddress the address of the trader
+    /// @return the address of the protocol governance contract
+    function traderIdByAddress(address traderAddress) external view returns (uint256);
+
+    function traderAddressById(uint256) external view returns (address);
+
+    function traders() external view returns (uint256[] memory);
+
+    function addTrader(address) external;
+
+    function removeTraderByAddress(address) external;
+
+    function removeTraderById(uint256) external;
+
+    function trade(
+        uint256 traderId,
+        address input,
+        address output,
+        uint256 amount,
+        SwapType swapType,
+        bytes calldata options
+    ) external returns (uint256);
 }
--- contracts/trader/MasterTrader.sol
+++ contracts/trader/MasterTrader.sol
@@ -5,41 +5,39 @@ import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "../interfaces/IProtocolGovernance.sol";
 import "./ITrader.sol";
+import "./IMasterTrader.sol";
 import "./TraderLibrary.sol";
 
-contract MasterTrader is ERC165 {
+contract MasterTrader is ERC165, IMasterTrader {
     using EnumerableSet for EnumerableSet.UintSet;
 
-    enum SwapType {
-        EXACT_INPUT_SINGLE,
-        EXACT_OUTPUT_SINGLE,
-        EXACT_INPUT_MULTIHOP,
-        EXACT_OUTPUT_MULTIHOP
-    }
-
-    IProtocolGovernance public immutable protocolGovernance;
+    address public immutable protocolGovernance;
     mapping(address => uint256) public traderIdByAddress;
     mapping(uint256 => address) public traderAddressById;
-    uint256 public topTraderId;
 
+    uint256 internal _topTraderId;
     EnumerableSet.UintSet internal _traders;
     mapping(SwapType => bytes4) internal _swapTypeToSelector;
 
     constructor(address _protocolGovernance) {
-        protocolGovernance = IProtocolGovernance(_protocolGovernance);
+        protocolGovernance = _protocolGovernance;
         _swapTypeToSelector[SwapType.EXACT_INPUT_SINGLE] = ITrader.swapExactInputSingle.selector;
         _swapTypeToSelector[SwapType.EXACT_OUTPUT_SINGLE] = ITrader.swapExactOutputSingle.selector;
         _swapTypeToSelector[SwapType.EXACT_INPUT_MULTIHOP] = ITrader.swapExactInputMultihop.selector;
         _swapTypeToSelector[SwapType.EXACT_OUTPUT_MULTIHOP] = ITrader.swapExactOutputMultihop.selector;
     }
 
+    function traders() external view returns (uint256[] memory) {
+        return _traders.values();
+    }
+
     function addTrader(address traderAddress) external {
         _requireProtocolAdmin();
         require(traderIdByAddress[traderAddress] == 0, TraderLibrary.TRADER_ALREADY_REGISTERED_EXCEPTION);
         // TODO: ERC165 interface check
-        traderIdByAddress[traderAddress] = ++topTraderId;
-        traderAddressById[topTraderId] = traderAddress;
-        _traders.add(topTraderId);
+        traderIdByAddress[traderAddress] = ++_topTraderId;
+        traderAddressById[_topTraderId] = traderAddress;
+        _traders.add(_topTraderId);
     }
 
     function removeTraderByAddress(address traderAddress) external {
@@ -60,10 +58,6 @@ contract MasterTrader is ERC165 {
         _traders.remove(traderId);
     }
 
-    function traders() external view returns (uint256[] memory) {
-        return _traders.values();
-    }
-
     function trade(
         uint256 traderId,
         address input,
@@ -71,7 +65,7 @@ contract MasterTrader is ERC165 {
         uint256 amount,
         SwapType swapType,
         bytes calldata options
-    ) external returns(uint256) {
+    ) external returns (uint256) {
         address traderAddress = traderAddressById[traderId];
         require(traderAddress != address(0), TraderLibrary.TRADER_NOT_FOUND_EXCEPTION);
         address recipient = msg.sender;
@@ -89,6 +83,9 @@ contract MasterTrader is ERC165 {
     }
 
     function _requireProtocolAdmin() internal view {
-        require(protocolGovernance.isAdmin(msg.sender), TraderLibrary.PROTOCOL_ADMIN_REQUIRED_EXCEPTION);
+        require(
+            IProtocolGovernance(protocolGovernance).isAdmin(msg.sender),
+            TraderLibrary.PROTOCOL_ADMIN_REQUIRED_EXCEPTION
+        );
     }
 }
