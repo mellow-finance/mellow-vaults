f3bf3579745e7105941dfeffeff28ef17882e353
AlexK <ak@mellow.finance>
2021-11-28 21:26:55 +0300

    interim

--- contracts/interfaces/IAaveVaultGovernance.sol
+++ contracts/interfaces/IAaveVaultGovernance.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.8.9;
 
 import "./external/aave/ILendingPool.sol";
--- contracts/interfaces/IUniV3VaultGovernance.sol
+++ contracts/interfaces/IUniV3VaultGovernance.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.9;
 
 import "./external/univ3/INonfungiblePositionManager.sol";
