6febd9e37863947a9c45efc0fae8ff349920ef85
Mikhail S <mihanix.it@gmail.com>
2021-11-29 14:24:07 +0300

    added case when passed invalid tokens to constructor

--- test/AaveVault.test.ts
+++ test/AaveVault.test.ts
@@ -2,7 +2,13 @@ import { expect } from "chai";
 import { deployments, getNamedAccounts, ethers } from "hardhat";
 import { AaveVault } from "./types/AaveVault";
 import { BigNumber } from "@ethersproject/bignumber";
-import { withSigner, depositW9, depositWBTC, sleep } from "./library/Helpers";
+import {
+    withSigner,
+    depositW9,
+    depositWBTC,
+    sleep,
+    randomAddress,
+} from "./library/Helpers";
 
 describe("AaveVault", () => {
     const aaveVaultNft: number = 1;
@@ -12,6 +18,8 @@ describe("AaveVault", () => {
     let aaveVault: string;
     let erc20Vault: string;
     let gatewayVault: string;
+    let wbtc: string;
+    let weth: string;
     let aaveVaultContract: AaveVault;
 
     before(async () => {
@@ -37,11 +45,42 @@ describe("AaveVault", () => {
                 "AaveVault",
                 aaveVault
             );
+            ({ wbtc, weth } = await getNamedAccounts());
         });
     });
 
     beforeEach(async () => {
         await deploymentFixture();
+    });
+
+    describe("#constructor", () => {
+        it("creates a new contract", async () => {
+            const { deploy, get } = deployments;
+            const { deployer } = await getNamedAccounts();
+            const vaultGovernance = await get("AaveVaultGovernance");
+            await deploy("AaveVault", {
+                from: deployer,
+                autoMine: true,
+                args: [vaultGovernance.address, [wbtc, weth]],
+            });
+        });
+
+        describe("when passed invalid tokens", () => {
+            it("reverts", async () => {
+                const { deploy, get } = deployments;
+                const { deployer } = await getNamedAccounts();
+                const vaultGovernance = await get("AaveVaultGovernance");
+                await expect(
+                    deploy("AaveVault", {
+                        from: deployer,
+                        args: [
+                            vaultGovernance.address,
+                            [randomAddress(), randomAddress()],
+                        ],
+                    })
+                ).to.be.reverted;
+            });
+        });
     });
 
     describe("#tvl", () => {
@@ -103,7 +142,6 @@ describe("AaveVault", () => {
             describe("happy case", () => {
                 it("approves deposits to lendingPool and updates tvl", async () => {
                     await withSigner(gatewayVault, async (signer) => {
-                        const { weth, wbtc } = await getNamedAccounts();
                         await expect(
                             aaveVaultContract
                                 .connect(signer)
@@ -118,7 +156,6 @@ describe("AaveVault", () => {
 
                 it("tvl raises with time", async () => {
                     await withSigner(gatewayVault, async (signer) => {
-                        const { weth, wbtc } = await getNamedAccounts();
                         await aaveVaultContract
                             .connect(signer)
                             .push([wbtc, weth], [amountWBTC, amount], []);
@@ -147,7 +184,6 @@ describe("AaveVault", () => {
                 it("not performs approve the second time", async () => {
                     const amount = ethers.utils.parseEther("1");
                     await withSigner(gatewayVault, async (signer) => {
-                        const { weth, wbtc } = await getNamedAccounts();
                         await expect(
                             aaveVaultContract
                                 .connect(signer)
@@ -189,7 +225,6 @@ describe("AaveVault", () => {
         describe("when nothing is pushed", () => {
             it("nothing is pulled", async () => {
                 await withSigner(gatewayVault, async (signer) => {
-                    const { weth, wbtc } = await getNamedAccounts();
                     await expect(
                         aaveVaultContract
                             .connect(signer)
@@ -211,7 +246,6 @@ describe("AaveVault", () => {
 
             beforeEach(async () => {
                 await deployments.fixture();
-                const { weth, wbtc } = await getNamedAccounts();
                 await depositW9(aaveVault, amount);
                 await withSigner(gatewayVault, async (signer) => {
                     await expect(
@@ -224,7 +258,6 @@ describe("AaveVault", () => {
 
             it("smth pulled", async () => {
                 await withSigner(gatewayVault, async (signer) => {
-                    const { weth, wbtc } = await getNamedAccounts();
                     await aaveVaultContract
                         .connect(signer)
                         .pull(erc20Vault, [wbtc, weth], [0, amount], []);
@@ -241,7 +274,6 @@ describe("AaveVault", () => {
             describe("when pull amount is greater then actual balance", () => {
                 it("executes", async () => {
                     await withSigner(gatewayVault, async (signer) => {
-                        const { weth, wbtc } = await getNamedAccounts();
                         await expect(
                             aaveVaultContract
                                 .connect(signer)
