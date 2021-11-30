66e0538b6f99e349395d4f5cbaeaac54d4081dd7
Mikhail S <mihanix.it@gmail.com>
2021-11-30 21:11:08 +0300

    hack VaultRegistry tests

--- test/library/Deployments.ts
+++ test/library/Deployments.ts
@@ -610,12 +610,6 @@ export async function deploySubVaultSystem(options: {
         const [deployer] = await ethers.getSigners();
         await vaultRegistry
             .connect(signer)
-            .registerVault(
-                await deployer.getAddress(),
-                await deployer.getAddress()
-            );
-        await vaultRegistry
-            .connect(signer)
             .registerVault(options.vaultOwner, options.vaultOwner);
     });
     // />
