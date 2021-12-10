96e2dd23844ac2ea375f6934735a6cfe8649596d
AlexK <ak@mellow.finance>
2021-12-11 01:13:19 +0300

    added m strategy initial params

--- deploy/012_MStrategy.ts
+++ deploy/012_MStrategy.ts
@@ -2,13 +2,21 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
-import { equals } from "ramda";
 import { setupVault, toObject } from "./000_utils";
+import { BigNumber } from "ethers";
+import { map } from "ramda";
 
 const deployMStrategy = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre;
     const { deploy, log, execute, read, get } = deployments;
-    const { deployer, mStrategyAdmin } = await getNamedAccounts();
+    const {
+        deployer,
+        mStrategyAdmin,
+        weth,
+        usdc,
+        uniswapV3Router,
+        uniswapV3PositionManager,
+    } = await getNamedAccounts();
 
     await deploy("MStrategyProxyAdmin", {
         from: deployer,
@@ -33,7 +41,7 @@ const deployMStrategy = async function (hre: HardhatRuntimeEnvironment) {
         log: true,
         autoMine: true,
     });
-    await deploy("MStrategyProxy", {
+    const proxyDeployment = await deploy("MStrategyProxy", {
         from: deployer,
         contract: "DefaultProxy",
         args: [mStrategyDeployment.address, mStrategyAdmin, []],
@@ -42,16 +50,91 @@ const deployMStrategy = async function (hre: HardhatRuntimeEnvironment) {
     });
 };
 
+const setupStrategy = async (
+    hre: HardhatRuntimeEnvironment,
+    mStrategy: string,
+    erc20Vault: string,
+    moneyVault: string
+) => {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy, log, execute, read, get } = deployments;
+
+    const { deployer, weth, usdc, uniswapV3Router, uniswapV3Factory } =
+        await getNamedAccounts();
+
+    const tokens = [weth, usdc].map((x) => x.toLowerCase()).sort();
+    const proxy = await get("MStrategyProxy");
+    const vaultCount = await read(
+        "MStrategy",
+        { to: proxy.address },
+        "vaultCount"
+    );
+    if (vaultCount.toNumber() === 0) {
+        console.log("Setting Stragy params");
+        const uniFactory = await hre.ethers.getContractAt(
+            "IUniswapV3Factory",
+            uniswapV3Factory
+        );
+        const uniV3Pool = await uniFactory.getPool(tokens[0], tokens[1], 3000);
+        const immutableParams = {
+            token0: tokens[0],
+            token1: tokens[1],
+            uniV3Pool,
+            uniV3Router: uniswapV3Router,
+            erc20Vault,
+            moneyVault,
+        };
+        const params = {
+            oraclePriceTimespan: 1800,
+            oracleLiquidityTimespan: 1800,
+            liquidToFixedRatioX96: BigNumber.from(2).pow(96 - 4),
+            sqrtPMinX96: BigNumber.from(
+                Math.round((1 / Math.sqrt(3000)) * 10 ** 6 * 2 ** 20)
+            ).mul(BigNumber.from(2).pow(76)),
+            sqrtPMaxX96: BigNumber.from(
+                Math.round((1 / Math.sqrt(5000)) * 10 ** 6 * 2 ** 20)
+            ).mul(BigNumber.from(2).pow(76)),
+            tokenRebalanceThresholdX96: BigNumber.from(
+                Math.round(1.1 * 2 ** 20)
+            ).mul(BigNumber.from(2).pow(76)),
+            poolRebalanceThresholdX96: BigNumber.from(
+                Math.round(1.1 * 2 ** 20)
+            ).mul(BigNumber.from(2).pow(76)),
+        };
+        log(
+            `Immutable Params:`,
+            map((x) => x.toString(), immutableParams)
+        );
+        log(
+            `Params:`,
+            map((x) => x.toString(), params)
+        );
+
+        await execute(
+            "MStrategy",
+            {
+                from: deployer,
+                log: true,
+                autoMine: true,
+                to: mStrategy,
+            },
+            "addVault",
+            immutableParams,
+            params
+        );
+    }
+};
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre;
     const { log, execute, read, get } = deployments;
-    const { deployer, mStrategyTreasury, weth, wbtc } =
+    const { deployer, mStrategyTreasury, weth, usdc } =
         await getNamedAccounts();
     const gatewayVaultGovernance = await get("GatewayVaultGovernance");
     const lpIssuerVaultGovernance = await get("LpIssuerGovernance");
     await deployMStrategy(hre);
 
-    const tokens = [weth, wbtc].map((t) => t.toLowerCase()).sort();
+    const tokens = [weth, usdc].map((t) => t.toLowerCase()).sort();
     const startNft =
         (await read("VaultRegistry", "vaultsCount")).toNumber() + 1;
     const coder = hre.ethers.utils.defaultAbiCoder;
@@ -156,6 +239,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         lpIssuer,
         lpIssuerNft
     );
+    const erc20Vault = await read(
+        "VaultRegistry",
+        "vaultForNft",
+        erc20VaultNft
+    );
+    const moneyVault = await read(
+        "VaultRegistry",
+        "vaultForNft",
+        yearnVaultNft
+    );
+    const mStrategy = await get("MStrategyProxy");
+    await setupStrategy(hre, mStrategy.address, erc20Vault, moneyVault);
 };
 
 export default func;
--- test/MStrategy.test.ts
+++ test/MStrategy.test.ts
@@ -1,14 +1,40 @@
+import { BigNumber } from "@ethersproject/bignumber";
 import { expect } from "chai";
-import { getNamedAccounts, ethers } from "hardhat";
-import { mint, randomAddress } from "./library/Helpers";
-import { ERC20 } from "./types";
+import { getNamedAccounts, ethers, deployments } from "hardhat";
+import { mint, randomAddress, withSigner } from "./library/Helpers";
+import { ERC20, LpIssuer } from "./types";
 
 describe("MStrategy", () => {
-    it("can mint", async () => {
-        const address = randomAddress();
-        await mint("WBTC", address, 1000);
-        const { wbtc } = await getNamedAccounts();
-        const c: ERC20 = await ethers.getContractAt("ERC20", wbtc);
-        expect(await c.balanceOf(address)).to.eq(1000);
+    let deploymentFixture: Function;
+    let tokens: string[];
+    let tokenContracts: ERC20[];
+
+    before(async () => {
+        deploymentFixture = await deployments.createFixture(async () => {
+            await deployments.fixture();
+            const { test } = await getNamedAccounts();
+
+            await withSigner(test, async (s) => {
+                const lpIssuer: LpIssuer = await ethers.getContract("LpIssuer");
+                tokens = await lpIssuer.vaultTokens();
+                const balances = [];
+                for (const token of tokens) {
+                    const c: ERC20 = await ethers.getContractAt("ERC20", token);
+                    tokenContracts.push(c);
+                    balances.push(await c.balanceOf(test));
+                }
+                await lpIssuer.deposit(
+                    balances.map((x: BigNumber) => x.div(2)),
+                    []
+                );
+            });
+        });
+    });
+    beforeEach(async () => {
+        await deploymentFixture();
+    });
+
+    describe("shouldRebalance", () => {
+        it("checks if the tokens needs to be rebalanced", async () => {});
     });
 });
