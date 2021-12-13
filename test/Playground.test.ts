3f71db043cf2cf738158d7813e8825bc4e3ef9be
AlexK <ak@mellow.finance>
2021-12-13 19:05:49 +0300

    added options

--- test/Playground.test.ts
+++ test/Playground.test.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from "@ethersproject/bignumber";
 import { pit, uint256 } from "./library/property";
 
-pit("works", uint256, uint256, async (x: BigNumber, y: BigNumber) => {
+pit("works", {}, uint256, uint256, async (x: BigNumber, y: BigNumber) => {
     return x.lte(y);
 });
--- test/library/property.ts
+++ test/library/property.ts
@@ -1,5 +1,18 @@
 import { BigNumber } from "@ethersproject/bignumber";
-import { Arbitrary, bigIntN, bigUintN, hexaString } from "fast-check";
+import {
+    Arbitrary,
+    assert,
+    asyncProperty,
+    AsyncPropertyHookFunction,
+    bigUintN,
+    hexaString,
+    Parameters,
+} from "fast-check";
+
+export type PropertyOptions = Parameters & {
+    beforeEach?: AsyncPropertyHookFunction;
+    afterEach?: AsyncPropertyHookFunction;
+};
 
 export const uint256: Arbitrary<BigNumber> = bigUintN(256).map((x: bigint) =>
     BigNumber.from(x.toString())
@@ -9,20 +22,27 @@ export const address: Arbitrary<string> = hexaString({ maxLength: 40 }).map(
     (x: string) => `0x${x}`
 );
 
-export function pit(description: string, f: () => Promise<boolean>): void;
+export function pit(
+    description: string,
+    options: PropertyOptions,
+    f: () => Promise<boolean>
+): void;
 export function pit<T0>(
     description: string,
+    options: PropertyOptions,
     a0: Arbitrary<T0>,
     f: (c0: T0) => Promise<boolean>
 ): void;
 export function pit<T0, T1>(
     description: string,
+    options: PropertyOptions,
     a0: Arbitrary<T0>,
     a1: Arbitrary<T1>,
     f: (c0: T0, c1: T1) => Promise<boolean>
 ): void;
 export function pit<T0, T1, T2>(
     description: string,
+    options: PropertyOptions,
     a0: Arbitrary<T0>,
     a1: Arbitrary<T1>,
     a2: Arbitrary<T2>,
@@ -31,6 +51,7 @@ export function pit<T0, T1, T2>(
 
 export function pit<T0, T1, T2, T3>(
     description: string,
+    options: PropertyOptions,
     a0: Arbitrary<T0>,
     a1: Arbitrary<T1>,
     a2: Arbitrary<T2>,
@@ -40,6 +61,7 @@ export function pit<T0, T1, T2, T3>(
 
 export function pit<T0, T1, T2, T3, T4>(
     description: string,
+    options: PropertyOptions,
     a0: Arbitrary<T0>,
     a1: Arbitrary<T1>,
     a2: Arbitrary<T2>,
@@ -48,9 +70,21 @@ export function pit<T0, T1, T2, T3, T4>(
     f: (c0: T0, c1: T1, c2: T2, c3: T3, c4: T4) => Promise<boolean>
 ): void;
 
-export function pit(description: string, ...args: any[]): void {
+export function pit(
+    description: string,
+    options: PropertyOptions,
+    ...args: any[]
+): void {
     it(`@property: ${description}`, async () => {
         // @ts-ignore
-        await fc.assert(fc.asyncProperty(...args));
+        let prop = asyncProperty(...args);
+        if (options.beforeEach) {
+            prop = prop.beforeEach(options.beforeEach);
+        }
+        if (options.afterEach) {
+            prop = prop.afterEach(options.afterEach);
+        }
+        // @ts-ignore
+        await assert(prop, options);
     });
 }
