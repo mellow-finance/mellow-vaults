e56d2b1d9a05c19ffbb357be0d744a939f387ff3
AlexK <ak@mellow.finance>
2021-12-14 01:00:42 +0300

    cleanup

--- test/Playground.test.ts
+++
@@ -1,6 +0,0 @@
-import { BigNumber } from "@ethersproject/bignumber";
-import { pit, uint256 } from "./library/property";
-
-pit("works", {}, uint256, uint256, async (x: BigNumber, y: BigNumber) => {
-    return x.lte(y);
-});
