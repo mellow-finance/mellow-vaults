0110987808db74c01dc57df54e78170a09207738
AlexK <ak@mellow.finance>
2021-12-14 19:23:39 +0300

    added props

--- test/behaviors/vaultGovernanceDelayedProtocolParams.ts
+++ test/behaviors/vaultGovernanceDelayedProtocolParams.ts
@@ -1,5 +1,5 @@
 import { BigNumber, Contract, ethers, Signer } from "ethers";
-import { Arbitrary, Random } from "fast-check";
+import { Arbitrary, nat, Random } from "fast-check";
 import { type } from "os";
 import {
     randomAddress,
@@ -14,6 +14,7 @@ import { expect } from "chai";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signers";
 import Exceptions from "../library/Exceptions";
 import { generateParams, VaultGovernanceContext } from "./vaultGovernance";
+import { deployments } from "hardhat";
 
 export function delayedProtocolParamsBehavior<P, S extends Contract>(
     this: VaultGovernanceContext<S>,
@@ -32,7 +33,7 @@ export function delayedProtocolParamsBehavior<P, S extends Contract>(
                 .stageDelayedProtocolParams(someParams);
             const actualParams =
                 await this.subject.stagedDelayedProtocolParams();
-            expect(actualParams).to.be.equivalent(someParams);
+            expect(someParams).to.be.equivalent(actualParams);
         });
 
         describe("properties", () => {
@@ -66,7 +67,7 @@ export function delayedProtocolParamsBehavior<P, S extends Contract>(
                 it("returns zero struct", async () => {
                     const actualParams =
                         await this.subject.stagedDelayedProtocolParams();
-                    expect(actualParams).to.equivalent(noneParams);
+                    expect(noneParams).to.equivalent(actualParams);
                 });
             });
 
@@ -81,7 +82,7 @@ export function delayedProtocolParamsBehavior<P, S extends Contract>(
                         .commitDelayedProtocolParams();
                     const actualParams =
                         await this.subject.stagedDelayedProtocolParams();
-                    expect(actualParams).to.equivalent(noneParams);
+                    expect(noneParams).to.equivalent(actualParams);
                 });
             });
         });
@@ -97,7 +98,7 @@ export function delayedProtocolParamsBehavior<P, S extends Contract>(
                 .connect(this.admin)
                 .commitDelayedProtocolParams();
             const actualParams = await this.subject.delayedProtocolParams();
-            expect(actualParams).to.equivalent(someParams);
+            expect(someParams).to.equivalent(actualParams);
         });
         describe("properties", () => {
             pit(
@@ -143,7 +144,7 @@ export function delayedProtocolParamsBehavior<P, S extends Contract>(
                 .stageDelayedProtocolParams(someParams);
             const actualParams =
                 await this.subject.stagedDelayedProtocolParams();
-            expect(actualParams).to.be.equivalent(someParams);
+            expect(someParams).to.be.equivalent(actualParams);
         });
         it("sets delay for commit", async () => {
             await this.subject
@@ -247,6 +248,198 @@ export function delayedProtocolParamsBehavior<P, S extends Contract>(
                     const actualParams =
                         await this.subject.stagedDelayedProtocolParams();
                     expect(noneParams).to.be.equivalent(actualParams);
+                });
+            });
+        });
+    });
+
+    describe("#commitDelayedProtocolParams", () => {
+        let stagedFixture: Function;
+        before(async () => {
+            stagedFixture = await deployments.createFixture(async () => {
+                await this.deploymentFixture();
+                await this.subject
+                    .connect(this.admin)
+                    .stageDelayedProtocolParams(someParams);
+            });
+        });
+        beforeEach(async () => {
+            await stagedFixture();
+            await sleep(this.governanceDelay);
+        });
+        it("commits staged DelayedProtocolParams", async () => {
+            await this.subject
+                .connect(this.admin)
+                .commitDelayedProtocolParams();
+
+            const actualParams = await this.subject.delayedProtocolParams();
+            expect(someParams).to.be.equivalent(actualParams);
+        });
+        it("resets delay for commit", async () => {
+            await this.subject
+                .connect(this.admin)
+                .commitDelayedProtocolParams();
+            expect(
+                await this.subject.delayedProtocolParamsTimestamp()
+            ).to.equal(BigNumber.from(0));
+        });
+        it("emits CommitDelayedProtocolParams event", async () => {
+            await expect(
+                await this.subject
+                    .connect(this.admin)
+                    .commitDelayedProtocolParams()
+            ).to.emit(this.subject, "CommitDelayedProtocolParams");
+        });
+
+        describe("properties", () => {
+            pit(
+                "cannot be called by random address",
+                { numRuns: RUNS.verylow },
+                address,
+                paramsArb,
+                async (addr: string, params: P) => {
+                    await this.subject
+                        .connect(this.admin)
+                        .stageDelayedProtocolParams(someParams);
+                    await sleep(this.governanceDelay);
+
+                    await withSigner(addr, async (s) => {
+                        await expect(
+                            this.subject
+                                .connect(s)
+                                .commitDelayedProtocolParams()
+                        ).to.be.revertedWith(Exceptions.ADMIN);
+                    });
+                    return true;
+                }
+            );
+            pit(
+                "reverts if called before the delay has elapsed",
+                { numRuns: RUNS.mid },
+                async () => nat((await this.governanceDelay) - 60),
+                paramsArb,
+                async (delay: number, params: P) => {
+                    await this.subject
+                        .connect(this.admin)
+                        .stageDelayedProtocolParams(someParams);
+                    await sleep(delay);
+
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.be.revertedWith(Exceptions.TIMESTAMP);
+                    return true;
+                }
+            );
+            pit(
+                "succeeds if called after the delay has elapsed",
+                { numRuns: RUNS.mid },
+                nat(),
+                paramsArb,
+                async (delay: number, params: P) => {
+                    await this.subject
+                        .connect(this.admin)
+                        .stageDelayedProtocolParams(someParams);
+                    await sleep(this.governanceDelay + 60 + delay);
+
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.not.be.reverted;
+                    return true;
+                }
+            );
+        });
+
+        describe("access control", () => {
+            it("allowed: ProtocolGovernance admin", async () => {
+                await this.subject
+                    .connect(this.admin)
+                    .commitDelayedProtocolParams();
+            });
+
+            it("denied: Vault NFT Owner (aka liquidity provider)", async () => {
+                await expect(
+                    this.subject
+                        .connect(this.ownerSigner)
+                        .commitDelayedProtocolParams()
+                ).to.be.revertedWith(Exceptions.ADMIN);
+            });
+            it("denied: Vault NFT Approved (aka strategy)", async () => {
+                await expect(
+                    this.subject
+                        .connect(this.strategySigner)
+                        .commitDelayedProtocolParams()
+                ).to.be.revertedWith(Exceptions.ADMIN);
+            });
+            it("denied: deployer", async () => {
+                await expect(
+                    this.subject
+                        .connect(this.deployer)
+                        .commitDelayedProtocolParams()
+                ).to.be.revertedWith(Exceptions.ADMIN);
+            });
+
+            it("denied: random address", async () => {
+                await withSigner(randomAddress(), async (s) => {
+                    await expect(
+                        this.subject
+                            .connect(this.strategySigner)
+                            .commitDelayedProtocolParams()
+                    ).to.be.revertedWith(Exceptions.ADMIN);
+                });
+            });
+        });
+
+        describe("edge cases", () => {
+            describe("when called twice", () => {
+                it("reverts", async () => {
+                    await this.subject
+                        .connect(this.admin)
+                        .commitDelayedProtocolParams();
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.be.revertedWith(Exceptions.NULL);
+                });
+            });
+            describe("when nothing is staged", () => {
+                it("reverts", async () => {
+                    await this.deploymentFixture();
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.be.revertedWith(Exceptions.NULL);
+                });
+            });
+            describe("when delay has not elapsed", () => {
+                it("reverts", async () => {
+                    await this.deploymentFixture();
+                    await this.subject
+                        .connect(this.admin)
+                        .stageDelayedProtocolParams(someParams);
+
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.be.revertedWith(Exceptions.TIMESTAMP);
+                    await sleep(this.governanceDelay - 60);
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.be.revertedWith(Exceptions.TIMESTAMP);
+                    await sleep(60);
+                    await expect(
+                        this.subject
+                            .connect(this.admin)
+                            .commitDelayedProtocolParams()
+                    ).to.not.be.reverted;
                 });
             });
         });
