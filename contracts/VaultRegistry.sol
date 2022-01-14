f259194aacd45d8d8f5efd8e008acc49a661902a
cube <cubetv228@gmail.com>
2022-01-14 14:24:29 +0300

    ensure only unique vaults can be registered

--- contracts/VaultRegistry.sol
+++ contracts/VaultRegistry.sol
@@ -18,6 +18,7 @@ contract VaultRegistry is IVaultRegistry, ERC721 {
     mapping(address => uint256) private _nftIndex;
     mapping(uint256 => address) private _vaultIndex;
     mapping(uint256 => bool) private _locks;
+    mapping(address => bool) private _vaultRegistered;
     uint256 private _topNft = 1;
 
     /// @notice Creates a new contract.
@@ -57,12 +58,14 @@ contract VaultRegistry is IVaultRegistry, ERC721 {
             _protocolGovernance.hasPermission(msg.sender, PermissionIdsLibrary.REGISTER_VAULT),
             ExceptionsLibrary.FORBIDDEN
         );
+        require(!_vaultRegistered[vault], ExceptionsLibrary.DUPLICATE);
         nft = _topNft;
         _safeMint(owner, nft);
         _vaultIndex[nft] = vault;
         _nftIndex[vault] = nft;
         _vaults.push(vault);
         _topNft += 1;
+        _vaultRegistered[vault] = true;
         emit VaultRegistered(tx.origin, msg.sender, nft, vault, owner);
     }
 
