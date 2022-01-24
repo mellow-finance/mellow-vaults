647eeb87b46bc6abd0c02d204d69a3944b7ef275
AlexK <ak@mellow.finance>
2022-01-24 12:05:05 +0300

    interim

--- contracts/libraries/SemverLibrary.sol
+++ contracts/libraries/SemverLibrary.sol
@@ -47,78 +47,95 @@ library SemverLibrary {
     }
 
     function numberifySemver(string memory _semver) internal pure returns (uint256) {
-        bytes memory semver = bytes(_semver);
-
-        uint8 BEFORE_NUMBER = 0;
-        uint8 IN_NUMBER = 1;
-        uint8 END_OF_NUMBER = 2;
-
-        uint8 state;
-        uint8 dotsCount;
-        uint8 lastDotPosition;
-
-        bytes memory num1 = new bytes(MAX_LENGTH);
-        bytes memory num2 = new bytes(MAX_LENGTH);
-        bytes memory num3 = new bytes(MAX_LENGTH);
-
-        for (uint8 i; i != semver.length; ++i) {
-            // switch state
-
-            // q0: [1-9] -> q1, [0] -> q2
-            if (state == BEFORE_NUMBER) {
-                if (isNumeric(semver[i]) && semver[i] != "0") {
-                    state = IN_NUMBER;
-                } else if (semver[i] == "0") {
-                    state = END_OF_NUMBER;
-                } else {
-                    return 0;
-                }
-                // q1: [0-9] -> q1, '.' -> q0
-            } else if (state == IN_NUMBER) {
-                if (isNumeric(semver[i])) {
-                    state = IN_NUMBER;
-                } else if (semver[i] == ".") {
-                    dotsCount++;
-                    lastDotPosition = i;
-                    state = BEFORE_NUMBER;
-                } else {
-                    return 0;
-                }
-                // q2: '.' -> q0
-            } else if (state == END_OF_NUMBER) {
-                if (semver[i] == ".") {
-                    dotsCount++;
-                    lastDotPosition = i;
-                    state = BEFORE_NUMBER;
-                } else {
-                    return 0;
-                }
+        uint256[3] memory res;
+        uint256 semveri;
+        for (uint256 i = 0; (i < bytes(_semver).length) && semveri < 3; i++) {
+            uint8 b = uint8(bytes(_semver)[i]);
+            if (b == uint8(bytes1("."))) {
+                semveri += 1;
+                continue;
             }
-
-            // construct the number
-
-            if (dotsCount > 2) {
+            if (b < ASCII_ZERO || b > ASCII_ZERO + 9) {
                 return 0;
             }
+            res[semveri] = res[semveri] * 10 + b - ASCII_ZERO;
+        }
+        if (semveri != 3) {
+            return 0;
+        }
+        return (res[0] << (16 + res[1])) << (8 + res[2]);
+        // bytes memory semver = bytes(_semver);
 
-            if (state != BEFORE_NUMBER) {
-                if (dotsCount == 0) {
-                    num1[i] = semver[i];
-                } else if (dotsCount == 1) {
-                    num2[i - lastDotPosition - 1] = semver[i];
-                } else {
-                    num3[i - lastDotPosition - 1] = semver[i];
-                }
-            }
-        }
-        if (dotsCount == 2 && state != BEFORE_NUMBER) {
-            uint256 result = numberify(shrinkToFit(num1)) << BIT_OFFSET;
-            result |= numberify(shrinkToFit(num2));
-            result <<= BIT_OFFSET;
-            result |= numberify(shrinkToFit(num3));
-            return result;
-        }
-        return 0;
+        // uint8 BEFORE_NUMBER = 0;
+        // uint8 IN_NUMBER = 1;
+        // uint8 END_OF_NUMBER = 2;
+
+        // uint8 state;
+        // uint8 dotsCount;
+        // uint8 lastDotPosition;
+
+        // bytes memory num1 = new bytes(MAX_LENGTH);
+        // bytes memory num2 = new bytes(MAX_LENGTH);
+        // bytes memory num3 = new bytes(MAX_LENGTH);
+
+        // for (uint8 i; i != semver.length; ++i) {
+        //     // switch state
+
+        //     // q0: [1-9] -> q1, [0] -> q2
+        //     if (state == BEFORE_NUMBER) {
+        //         if (isNumeric(semver[i]) && semver[i] != "0") {
+        //             state = IN_NUMBER;
+        //         } else if (semver[i] == "0") {
+        //             state = END_OF_NUMBER;
+        //         } else {
+        //             return 0;
+        //         }
+        //         // q1: [0-9] -> q1, '.' -> q0
+        //     } else if (state == IN_NUMBER) {
+        //         if (isNumeric(semver[i])) {
+        //             state = IN_NUMBER;
+        //         } else if (semver[i] == ".") {
+        //             dotsCount++;
+        //             lastDotPosition = i;
+        //             state = BEFORE_NUMBER;
+        //         } else {
+        //             return 0;
+        //         }
+        //         // q2: '.' -> q0
+        //     } else if (state == END_OF_NUMBER) {
+        //         if (semver[i] == ".") {
+        //             dotsCount++;
+        //             lastDotPosition = i;
+        //             state = BEFORE_NUMBER;
+        //         } else {
+        //             return 0;
+        //         }
+        //     }
+
+        //     // construct the number
+
+        //     if (dotsCount > 2) {
+        //         return 0;
+        //     }
+
+        //     if (state != BEFORE_NUMBER) {
+        //         if (dotsCount == 0) {
+        //             num1[i] = semver[i];
+        //         } else if (dotsCount == 1) {
+        //             num2[i - lastDotPosition - 1] = semver[i];
+        //         } else {
+        //             num3[i - lastDotPosition - 1] = semver[i];
+        //         }
+        //     }
+        // }
+        // if (dotsCount == 2 && state != BEFORE_NUMBER) {
+        //     uint256 result = numberify(shrinkToFit(num1)) << BIT_OFFSET;
+        //     result |= numberify(shrinkToFit(num2));
+        //     result <<= BIT_OFFSET;
+        //     result |= numberify(shrinkToFit(num3));
+        //     return result;
+        // }
+        // return 0;
     }
 
     function stringifySemver(uint256 num) internal pure returns (string memory) {
