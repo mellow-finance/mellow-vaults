45533b723ffd8139e944a519be12c705da14dd95
AlexK <ak@mellow.finance>
2022-01-26 00:20:53 +0300

    Added batch call

--- contracts/utils/BatchCall.sol
+++ contracts/utils/BatchCall.sol
@@ -4,8 +4,8 @@ pragma solidity =0.8.9;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "../libraries/ExceptionsLibrary.sol";
 
-contract MultiCall {
-    function multicall(address[] calldata targets, bytes[] calldata data) external returns (bytes[] memory results) {
+contract BatchCall {
+    function batchcall(address[] calldata targets, bytes[] calldata data) external returns (bytes[] memory results) {
         require(targets.length == data.length, ExceptionsLibrary.INVALID_LENGTH);
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
