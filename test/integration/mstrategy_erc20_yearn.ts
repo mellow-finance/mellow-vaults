49807c7e31d15b2b17336b051ad97f022152605b
Mikhail S <mihanix.it@gmail.com>
2022-02-01 09:26:43 +0300

    add mstrategy test deployment

---
+++ test/integration/mstrategy_erc20_yearn.ts
@@ -0,0 +1,181 @@
+import hre from "hardhat";
+import { ethers, deployments, getNamedAccounts } from "hardhat";
+import { BigNumber } from "@ethersproject/bignumber";
+import { mint } from "../library/Helpers";
+import { contract } from "../library/setup";
+import { ERC20RootVault, YearnVault, ERC20Vault, MStrategy } from "../types";
+import { setupVault, combineVaults } from "../../deploy/0000_utils";
+import { expect } from "chai";
+import { Contract } from "@ethersproject/contracts";
+
+type CustomContext = {
+    erc20Vault: ERC20Vault;
+    yearnVault: YearnVault;
+    erc20RootVault: ERC20RootVault;
+    positionManager: Contract;
+};
+
+type DeployOptions = {};
+
+contract<MStrategy, DeployOptions, CustomContext>(
+    "Integration__mstrategy",
+    function () {
+        before(async () => {
+            this.deploymentFixture = deployments.createFixture(
+                async (_, __?: DeployOptions) => {
+                    const { read } = deployments;
+
+                    const tokens = [this.weth.address, this.usdc.address]
+                        .map((t) => t.toLowerCase())
+                        .sort();
+                    const startNft =
+                        (
+                            await read("VaultRegistry", "vaultsCount")
+                        ).toNumber() + 1;
+
+                    let yearnVaultNft = startNft;
+                    let erc20VaultNft = startNft + 1;
+                    await setupVault(
+                        hre,
+                        yearnVaultNft,
+                        "YearnVaultGovernance",
+                        {
+                            createVaultArgs: [tokens, this.deployer.address],
+                        }
+                    );
+                    await setupVault(
+                        hre,
+                        erc20VaultNft,
+                        "ERC20VaultGovernance",
+                        {
+                            createVaultArgs: [tokens, this.deployer.address],
+                        }
+                    );
+
+                    const erc20Vault = await read(
+                        "VaultRegistry",
+                        "vaultForNft",
+                        erc20VaultNft
+                    );
+                    const yearnVault = await read(
+                        "VaultRegistry",
+                        "vaultForNft",
+                        yearnVaultNft
+                    );
+                    const erc20RootVault = await read(
+                        "VaultRegistry",
+                        "vaultForNft",
+                        erc20VaultNft + 1
+                    );
+
+                    this.erc20RootVault = await ethers.getContractAt(
+                        "ERC20RootVault",
+                        erc20RootVault
+                    );
+                    this.erc20Vault = await ethers.getContractAt(
+                        "ERC20Vault",
+                        erc20Vault
+                    );
+                    this.yearnVault = await ethers.getContractAt(
+                        "YearnVault",
+                        yearnVault
+                    );
+
+                    const { uniswapV3PositionManager, uniswapV3Router } =
+                        await getNamedAccounts();
+
+                    const mStrategy = await (
+                        await ethers.getContractFactory("MStrategy")
+                    ).deploy(uniswapV3PositionManager, uniswapV3Router);
+
+                    const params = [
+                        tokens,
+                        erc20Vault,
+                        yearnVault,
+                        3000,
+                        this.mStrategyAdmin.address,
+                    ];
+                    const address = await mStrategy.callStatic.createStrategy(
+                        ...params
+                    );
+                    await mStrategy.createStrategy(...params);
+                    this.subject = await ethers.getContractAt(
+                        "MStrategy",
+                        address
+                    );
+
+                    const oracleParams = {
+                        oracleObservationDelta: 15,
+                        maxTickDeviation: 50,
+                        maxSlippageD: Math.round(0.1 * 10 ** 9),
+                    };
+                    const ratioParams = {
+                        tickMin: 198240 - 5000,
+                        tickMax: 198240 + 5000,
+                        erc20MoneyRatioD: Math.round(0.1 * 10 ** 9),
+                    };
+                    const txs = [];
+                    txs.push(
+                        this.subject.interface.encodeFunctionData(
+                            "setOracleParams",
+                            [oracleParams]
+                        )
+                    );
+                    txs.push(
+                        this.subject.interface.encodeFunctionData(
+                            "setRatioParams",
+                            [ratioParams]
+                        )
+                    );
+
+                    await this.subject
+                        .connect(this.mStrategyAdmin)
+                        .functions["multicall"](txs);
+
+                    await combineVaults(
+                        hre,
+                        erc20VaultNft + 1,
+                        [erc20VaultNft, yearnVaultNft],
+                        this.subject.address,
+                        this.deployer.address
+                    );
+
+                    // add depositor
+                    await this.erc20RootVault
+                        .connect(this.admin)
+                        .addDepositorsToAllowlist([this.deployer.address]);
+
+                    await mint(
+                        "USDC",
+                        this.deployer.address,
+                        BigNumber.from(10).pow(6).mul(3000)
+                    );
+                    await mint(
+                        "WETH",
+                        this.deployer.address,
+                        BigNumber.from(10).pow(18)
+                    );
+
+                    await this.weth.approve(
+                        this.subject.address,
+                        ethers.constants.MaxUint256
+                    );
+                    await this.usdc.approve(
+                        this.subject.address,
+                        ethers.constants.MaxUint256
+                    );
+
+                    return this.subject;
+                }
+            );
+        });
+
+        beforeEach(async () => {
+            await this.deploymentFixture();
+        });
+
+        xit("rebalances", async () => {
+            await this.subject.connect(this.mStrategyAdmin).rebalance();
+        });
+    }
+);
