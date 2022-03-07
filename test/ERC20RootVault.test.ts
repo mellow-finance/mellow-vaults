fa3deaf543dba09a4e76ebd08d70e83c44d2ba72
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2022-03-07 14:20:43 +0400

    before initialized

---
+++ test/ERC20RootVault.test.ts
@@ -0,0 +1,155 @@
+import hre from "hardhat";
+import { expect } from "chai";
+import { ethers, getNamedAccounts, deployments } from "hardhat";
+import { BigNumber } from "@ethersproject/bignumber";
+import {
+    mint,
+    mintUniV3Position_USDC_WETH,
+} from "./library/Helpers";
+import { contract } from "./library/setup";
+import { ERC20RootVault, ERC20Vault, UniV3Vault } from "./types";
+import {
+    combineVaults,
+    setupVault,
+} from "../deploy/0000_utils";
+import { abi as INonfungiblePositionManager } from "@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json";
+import { abi as ISwapRouter } from "@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json";
+import Exceptions from "./library/Exceptions";
+
+type CustomContext = {
+    erc20Vault: ERC20Vault;
+    uniV3Vault: UniV3Vault;
+    curveRouter: string;
+    preparePush: () => any;
+};
+
+type DeployOptions = {};
+
+contract<ERC20RootVault, DeployOptions, CustomContext>("ERC20RootVault", function () {
+    const uniV3PoolFee = 3000;
+
+    before(async () => {
+        this.deploymentFixture = deployments.createFixture(
+            async (_, __?: DeployOptions) => {
+                await deployments.fixture();
+                const { read } = deployments;
+
+                const { uniswapV3PositionManager, curveRouter } =
+                    await getNamedAccounts();
+                this.curveRouter = curveRouter;
+
+                this.positionManager = await ethers.getContractAt(
+                    INonfungiblePositionManager,
+                    uniswapV3PositionManager
+                );
+
+                this.preparePush = async () => {
+                    const result = await mintUniV3Position_USDC_WETH({
+                        fee: 3000,
+                        tickLower: -887220,
+                        tickUpper: 887220,
+                        usdcAmount: BigNumber.from(10).pow(6).mul(3000),
+                        wethAmount: BigNumber.from(10).pow(18),
+                    });
+                    await this.positionManager.functions[
+                        "safeTransferFrom(address,address,uint256)"
+                    ](
+                        this.deployer.address,
+                        this.uniV3Vault.address,
+                        result.tokenId
+                    );
+                };
+
+                const tokens = [this.weth.address, this.usdc.address]
+                    .map((t) => t.toLowerCase())
+                    .sort();
+
+                const startNft =
+                    (await read("VaultRegistry", "vaultsCount")).toNumber() + 1;
+
+                let uniV3VaultNft = startNft;
+                let erc20VaultNft = startNft + 1;
+
+                await setupVault(hre, uniV3VaultNft, "UniV3VaultGovernance", {
+                    createVaultArgs: [
+                        tokens,
+                        this.deployer.address,
+                        uniV3PoolFee,
+                    ],
+                });
+                await setupVault(hre, erc20VaultNft, "ERC20VaultGovernance", {
+                    createVaultArgs: [tokens, this.deployer.address],
+                });
+
+                await combineVaults(
+                    hre,
+                    erc20VaultNft + 1,
+                    [erc20VaultNft, uniV3VaultNft],
+                    this.deployer.address,
+                    this.deployer.address
+                );
+                const erc20Vault = await read(
+                    "VaultRegistry",
+                    "vaultForNft",
+                    erc20VaultNft
+                );
+                const uniV3Vault = await read(
+                    "VaultRegistry",
+                    "vaultForNft",
+                    uniV3VaultNft
+                );
+                const erc20RootVault = await read(
+                    "VaultRegistry",
+                    "vaultForNft",
+                    erc20VaultNft + 1
+                );
+
+                this.erc20Vault = await ethers.getContractAt(
+                    "ERC20Vault",
+                    erc20Vault
+                );
+
+                this.uniV3Vault = await ethers.getContractAt(
+                    "UniV3Vault",
+                    uniV3Vault
+                );
+
+                this.subject = await ethers.getContractAt(
+                    "ERC20RootVault",
+                    erc20RootVault
+                );
+
+                for (let address of [
+                    this.deployer.address,
+                    this.uniV3Vault.address,
+                    this.erc20Vault.address,
+                ]) {
+                    await mint(
+                        "USDC",
+                        address,
+                        BigNumber.from(10).pow(18).mul(3000)
+                    );
+                    await mint(
+                        "WETH",
+                        address,
+                        BigNumber.from(10).pow(18).mul(3000)
+                    );
+                    await this.weth.approve(
+                        address,
+                        ethers.constants.MaxUint256
+                    );
+                    await this.usdc.approve(
+                        address,
+                        ethers.constants.MaxUint256
+                    );
+                }
+
+                return this.subject;
+            }
+        );
+    });
+
+    beforeEach(async () => {
+        await this.deploymentFixture();
+    });
+});
