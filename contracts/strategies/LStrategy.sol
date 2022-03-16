770b6f9b316bd1f27b7ae6182ade17df2c37a495
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2022-03-16 15:49:36 +0400

    fix: Incorrect Parameters on externalCall at LStrategy:postPreOrder

--- contracts/strategies/LStrategy.sol
+++ contracts/strategies/LStrategy.sol
@@ -374,7 +374,7 @@ contract LStrategy is ContractMeta, Multicall, DefaultAccessControl {
         require(order.validTo <= preOrder_.deadline, ExceptionsLibrary.TIMESTAMP);
         bytes memory approveData = abi.encode(cowswap, order.sellAmount);
         erc20Vault.externalCall(address(order.sellToken), APPROVE_SELECTOR, approveData);
-        bytes memory setPresignatureData = abi.encode(SET_PRESIGNATURE_SELECTOR, uuid, signed);
+        bytes memory setPresignatureData = abi.encode(uuid, signed);
         erc20Vault.externalCall(cowswap, SET_PRESIGNATURE_SELECTOR, setPresignatureData);
         orderDeadline = order.validTo;
         delete preOrder;
