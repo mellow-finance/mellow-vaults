a0dc5a69042dac80a142cc24f71616dfe6043fae
Alexey Saplin <40490358+AlexSaplin@users.noreply.github.com>
2022-04-11 12:12:11 +0400

    prettier

--- test/library/Constants.ts
+++ test/library/Constants.ts
@@ -9,5 +9,5 @@ export const UNIV3_VAULT_INTERFACE_ID: string = "0x58742a93";
 export const AAVE_VAULT_INTERFACE_ID: string = "0x0cba2eff";
 export const YEARN_VAULT_INTERFACE_ID: string = "0xa920b78c";
 export const PROTOCOL_GOVERNANCE_INTERFACE_ID: string = "0xca11fe03";
-export const ERC20_ROOT_VAULT_INTERFACE_ID: string = "0x7681ee51";
+export const ERC20_ROOT_VAULT_INTERFACE_ID: string = "0x06220456";
 export const ERC20_ROOT_VAULT_GOVERNANCE: string = "0x38e18cd8";
