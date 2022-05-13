757135be2c04d320ebffc7eaea2e7e07ccabb01d
Artyom Yusupov <iusupov.ai@phystech.edu>
2022-05-13 16:07:51 +0400

    fix: removed logging from UniV3 Vault

--- contracts/vaults/UniV3Vault.sol
+++ contracts/vaults/UniV3Vault.sol
@@ -11,7 +11,6 @@ import "../libraries/external/TickMath.sol";
 import "../libraries/external/LiquidityAmounts.sol";
 import "../libraries/ExceptionsLibrary.sol";
 import "./IntegrationVault.sol";
-import "hardhat/console.sol";
 
 /// @notice Vault that interfaces UniswapV3 protocol in the integration layer.
 contract UniV3Vault is IUniV3Vault, IntegrationVault {
@@ -213,8 +212,6 @@ contract UniV3Vault is IUniV3Vault, IntegrationVault {
         returns (uint256[] memory actualTokenAmounts)
     {
         actualTokenAmounts = new uint256[](2);
-        console.log("Push tokenAmount0: ", tokenAmounts[0]);
-        console.log("Push tokenAmount1: ", tokenAmounts[1]);
         if (uniV3Nft == 0) return actualTokenAmounts;
 
         address[] memory tokens = _vaultTokens;
