dd00a6a334b16a599f358dcc9b3de86f1aea959e
Artyom Yusupov <iusupov.ai@phystech.edu>
2022-05-27 20:15:35 +0400

    fix: removed unused method (commiting yTokens)

--- contracts/vaults/YearnVaultGovernance.sol
+++ contracts/vaults/YearnVaultGovernance.sol
@@ -10,7 +10,6 @@ import "./VaultGovernance.sol";
 /// @notice Governance that manages all Aave Vaults params and can deploy a new Aave Vault.
 contract YearnVaultGovernance is ContractMeta, IYearnVaultGovernance, VaultGovernance {
     mapping(address => address) private _yTokens;
-    bool public tokensCommited;
 
     /// @notice Creates a new contract
     /// @param internalParams_ Initial Internal Params
@@ -78,7 +77,6 @@ contract YearnVaultGovernance is ContractMeta, IYearnVaultGovernance, VaultGover
     /// @inheritdoc IYearnVaultGovernance
     function setYTokenForToken(address token, address yToken) external {
         _requireProtocolAdmin();
-        require(!tokensCommited, ExceptionsLibrary.FORBIDDEN);
         _yTokens[token] = yToken;
         emit SetYToken(tx.origin, msg.sender, token, yToken);
     }
@@ -93,10 +91,6 @@ contract YearnVaultGovernance is ContractMeta, IYearnVaultGovernance, VaultGover
         vault = IYearnVault(vaddr);
         vault.initialize(nft, vaultTokens_);
         emit DeployedVault(tx.origin, msg.sender, vaultTokens_, "", owner_, vaddr, nft);
-    }
-
-    function commitYTokens() external {
-        tokensCommited = true;
     }
 
     // -------------------  INTERNAL, VIEW  -------------------
