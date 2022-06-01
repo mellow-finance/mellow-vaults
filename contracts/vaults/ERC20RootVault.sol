a463f3a0a14ca47ca5eef3a56c47ed3263853b2d
Andrey Borzenkov <armoking@mellow.finance>
2022-06-01 20:14:33 +0400

    fix for price calculations in function _getTvlToken0 in ERC20RootVault

--- contracts/vaults/ERC20RootVault.sol
+++ contracts/vaults/ERC20RootVault.sol
@@ -213,14 +213,14 @@ contract ERC20RootVault is IERC20RootVault, ERC20Token, ReentrancyGuard, Aggrega
     ) internal view returns (uint256 tvl0) {
         tvl0 = tvls[0];
         for (uint256 i = 1; i < tvls.length; i++) {
-            (uint256[] memory prices, ) = oracle.price(tokens[0], tokens[i], 0x30);
-            require(prices.length > 0, ExceptionsLibrary.VALUE_ZERO);
-            uint256 price = 0;
-            for (uint256 j = 0; j < prices.length; j++) {
-                price += prices[j];
+            (uint256[] memory pricesX96, ) = oracle.price(tokens[0], tokens[i], 0x30);
+            require(pricesX96.length > 0, ExceptionsLibrary.VALUE_ZERO);
+            uint256 priceX96 = 0;
+            for (uint256 j = 0; j < pricesX96.length; j++) {
+                priceX96 += pricesX96[j];
             }
-            price /= prices.length;
-            tvl0 += tvls[i] / price;
+            priceX96 /= pricesX96.length;
+            tvl0 += FullMath.mulDiv(tvls[i], CommonLibrary.Q96, priceX96);
         }
     }
 
