9f2db3b40f70462fa9b221065c672ca887d1c60f
YusupovAI <44296501+YusupovAI@users.noreply.github.com>
2022-06-06 01:03:13 +0400

    Fixes/lstrategy helper (#165)
    fixes: rebalance case zero univ3liquidity fixed and tests added
    fixes: added checks in LStrategy
    fixes: added different fees to tokens
    fixes: deploy parameters added
    fixes: extended helper to reduce lstrategy size
    fixes: helper checkOrder
    fixes: added returning to parameters in order to make static calls
    tests: added tests for LStrategy rebalance returning values

--- contracts/utils/LStrategyHelper.sol
+++ contracts/utils/LStrategyHelper.sol
@@ -2,11 +2,13 @@
 pragma solidity 0.8.9;
 
 import "../interfaces/external/cowswap/ICowswapSettlement.sol";
-import "../interfaces/utils/ILStrategyOrderHelper.sol";
+import "../interfaces/utils/ILStrategyHelper.sol";
 import "../libraries/external/GPv2Order.sol";
+import "../libraries/CommonLibrary.sol";
 import "../libraries/ExceptionsLibrary.sol";
+import "../libraries/external/TickMath.sol";
 
-contract LStrategyOrderHelper is ILStrategyOrderHelper {
+contract LStrategyHelper is ILStrategyHelper {
     // IMMUTABLES
     address public immutable cowswap;
 
@@ -24,7 +26,8 @@ contract LStrategyOrderHelper is ILStrategyOrderHelper {
         uint256 amountIn,
         uint256 minAmountOut,
         uint256 deadline,
-        address erc20Vault
+        address erc20Vault,
+        uint256 fee
     ) external view {
         require(deadline >= block.timestamp, ExceptionsLibrary.TIMESTAMP);
         (bytes32 orderHashFromUid, , ) = GPv2Order.extractOrderUidParams(uuid);
@@ -37,5 +40,14 @@ contract LStrategyOrderHelper is ILStrategyOrderHelper {
         require(order.buyAmount >= minAmountOut, ExceptionsLibrary.LIMIT_UNDERFLOW);
         require(order.validTo <= deadline, ExceptionsLibrary.TIMESTAMP);
         require(order.receiver == erc20Vault, ExceptionsLibrary.FORBIDDEN);
+        require(order.kind == GPv2Order.KIND_SELL, ExceptionsLibrary.INVALID_VALUE);
+        require(order.sellTokenBalance == GPv2Order.BALANCE_ERC20, ExceptionsLibrary.INVALID_VALUE);
+        require(order.buyTokenBalance == GPv2Order.BALANCE_ERC20, ExceptionsLibrary.INVALID_VALUE);
+        require(order.feeAmount <= fee, ExceptionsLibrary.INVALID_VALUE);
+    }
+
+    function tickFromPriceX96(uint256 priceX96) external pure returns (int24) {
+        uint256 sqrtPriceX96 = CommonLibrary.sqrtX96(priceX96);
+        return TickMath.getTickAtSqrtRatio(uint160(sqrtPriceX96));
     }
 }
