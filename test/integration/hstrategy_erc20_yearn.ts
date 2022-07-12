37bd484bb336ab1dcfde43efe85b70799adfa4ce
Andrey Borzenkov <armoking32@gmail.com>
2022-07-13 00:32:39 +0400

    revert changes in tests

--- test/integration/hstrategy_erc20_yearn.ts
+++ test/integration/hstrategy_erc20_yearn.ts
@@ -519,12 +519,12 @@ contract<MockHStrategy, DeployOptions, CustomContext>(
             return BigNumber.from(sqrtPriceX96).pow(2).div(Q96);
         };
 
-        describe.only("#rebalance", () => {
+        describe("#rebalance", () => {
             const getPriceX96 = (tick: number) => {
                 return this.getSqrtRatioAtTick(tick).pow(2).div(Q96);
             };
 
-            it("`tvl chanages only on fees`", async () => {
+            it("tvl chanages only on fees", async () => {
                 const centralTick = await getAverageTick();
                 const globalLowerTick =
                     centralTick - 6000 - (centralTick % 600);
