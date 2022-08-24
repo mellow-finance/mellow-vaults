fba72505dfeef4ad25af4e5102a6ffd6b659af64
Artyom Yusupov <iusupov.ai@phystech.edu>
2022-08-24 18:13:05 +0400

    feature: first version of the oracle for wsteth / usd

---
+++ test/WstethPriceFees.test.ts
@@ -0,0 +1,7 @@
+import hre from "hardhat";
+import { expect } from "chai";
+import { ethers, getNamedAccounts, deployments } from "hardhat";
+import { BigNumber } from "@ethersproject/bignumber";
+
+import { AggregatorV3wstEth } from "./types";
+
