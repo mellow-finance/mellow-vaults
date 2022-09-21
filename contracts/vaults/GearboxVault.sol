114b7928f32efd4f3f9e82d1524af64190ef6a92
Dmitry Grigoryev <mitia810@mail.ru>
2022-09-21 19:04:17 +0300

    prettified

--- contracts/vaults/GearboxVault.sol
+++ contracts/vaults/GearboxVault.sol
@@ -33,11 +33,9 @@ contract GearboxVault is IGearboxVault, IntegrationVault {
 
     uint256 marginalFactorD;
 
-    mapping (address => uint256) public lpTokensToWithdrawOrder;
-    mapping (address => uint256) public lastOrderTimestamp;
+    mapping(address => uint256) public lpTokensToWithdrawOrder;
+    mapping(address => uint256) public lastOrderTimestamp;
     uint256 lastWithdrawResetTimestamp;
-    
-
 
     function tvl() public view override returns (uint256[] memory minTokenAmounts, uint256[] memory maxTokenAmounts) {
         (uint256 total, ) = _creditFacade.calcTotalValue(creditAccount);
