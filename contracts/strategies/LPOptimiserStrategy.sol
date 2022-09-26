ad213a424d8d8c7577c38b066547b1de216e1f04
tudor47 <tudorcostin7@gmail.com>
2022-09-26 18:31:09 +0300

    fix: lint

--- contracts/strategies/LPOptimiserStrategy.sol
+++ contracts/strategies/LPOptimiserStrategy.sol
@@ -50,7 +50,7 @@ contract LPOptimiserStrategy is DefaultAccessControl {
     event Rebalanced(int24 newTickLowerMul, int24 newTickUpperMul);
 
     // allows you to keep track of the smart contract activity in subgraph
-    event StrategyDeployment(IERC20Vault erc20vault_, IVoltzVault vault_, address admin_); 
+    event StrategyDeployment(IERC20Vault erc20vault_, IVoltzVault vault_, address admin_);
 
     /// @notice Constructor for a new contract
     /// @param erc20vault_ Reference to ERC20 Vault
@@ -71,8 +71,13 @@ contract LPOptimiserStrategy is DefaultAccessControl {
 
         emit StrategyDeployment(erc20vault_, vault_, admin_);
     }
-    
-    function setConstants(int24 logProx, uint256 sigmaWad, int256 max_possible_lower_bound_wad, int24 tickSpacing) public {
+
+    function setConstants(
+        int24 logProx,
+        uint256 sigmaWad,
+        int256 max_possible_lower_bound_wad,
+        int24 tickSpacing
+    ) public {
         _requireAtLeastOperator();
         _logProximity = logProx;
         _sigmaWad = sigmaWad;
@@ -80,7 +85,11 @@ contract LPOptimiserStrategy is DefaultAccessControl {
         _tickSpacing = tickSpacing;
     }
 
-    function setTickValues(int24 currentTick, int24 tickLower, int24 tickUpper) public {
+    function setTickValues(
+        int24 currentTick,
+        int24 tickLower,
+        int24 tickUpper
+    ) public {
         _requireAtLeastOperator();
         _currentTick = currentTick;
         _tickLower = tickLower;
@@ -89,7 +98,6 @@ contract LPOptimiserStrategy is DefaultAccessControl {
 
     /// @notice Get the current tick and position ticks and decide whether to rebalance
     function rebalanceCheck() public view returns (bool) {
-        
         // 1. Get current position, lower, and upper ticks
         // _currentPosition = _vault.currentPosition();
         // _tickLower = _currentPosition.tickLower;
@@ -99,11 +107,7 @@ contract LPOptimiserStrategy is DefaultAccessControl {
         // _currentTick = _periphery.getCurrentTick(_marginEngine);
 
         // 3. Compare current fixed rate to lower and upper bounds
-        if (
-            _tickLower - _logProximity <= _currentTick &&
-            _currentTick <= _tickUpper + _logProximity
-            
-            ) {
+        if (_tickLower - _logProximity <= _currentTick && _currentTick <= _tickUpper + _logProximity) {
             // 4.1. If current fixed rate is within bounds, return false (don't rebalance)
             return false;
         } else {
@@ -114,58 +118,64 @@ contract LPOptimiserStrategy is DefaultAccessControl {
 
     /// @notice Get the nearest tick multiple given a tick and tick spacing
     function nearestTickMultiple(int24 newTick, int24 tickSpacing) public pure returns (int24) {
-     return (newTick / tickSpacing + ( ( ((newTick % tickSpacing) + tickSpacing) % tickSpacing ) >= tickSpacing/2 ? int24(1) : int24(0)) ) * tickSpacing;
+        return
+            (newTick /
+                tickSpacing +
+                ((((newTick % tickSpacing) + tickSpacing) % tickSpacing) >= tickSpacing / 2 ? int24(1) : int24(0))) *
+            tickSpacing;
     }
 
     /// @notice Set new optimimal tick range based on current tick
     /// @param currentFixedRateWad currentFixedRate which is passed in from a 7-day rolling avg. historical fixed rate.
-    function rebalance (uint256 currentFixedRateWad) public returns (int24 newTickLowerMul, int24 newTickUpperMul) {
+    function rebalance(uint256 currentFixedRateWad) public returns (int24 newTickLowerMul, int24 newTickUpperMul) {
         _requireAtLeastOperator();
 
-            // 0. Get tickspacing from vamm
-            // _tickSpacing = _vamm.tickSpacing(_vamm);
+        // 0. Get tickspacing from vamm
+        // _tickSpacing = _vamm.tickSpacing(_vamm);
 
-            // 1. Get the new tick lower
-            int256 deltaWad = int256(currentFixedRateWad - _sigmaWad);
-            int256 newFixedLowerWad =  0;
-            if (deltaWad > 0) {
-                // delta is greater than 0 => choose delta
-                if (deltaWad < _max_possible_lower_bound_wad) {
-                    newFixedLowerWad = deltaWad;
-                } else {
-                    newFixedLowerWad = _max_possible_lower_bound_wad;
-                }
+        // 1. Get the new tick lower
+        int256 deltaWad = int256(currentFixedRateWad - _sigmaWad);
+        int256 newFixedLowerWad = 0;
+        if (deltaWad > 0) {
+            // delta is greater than 0 => choose delta
+            if (deltaWad < _max_possible_lower_bound_wad) {
+                newFixedLowerWad = deltaWad;
             } else {
-                // delta is less than or equal to 0 => choose 0
-                newFixedLowerWad = 0;
+                newFixedLowerWad = _max_possible_lower_bound_wad;
             }
-            // 2. Get the new tick upper
-            int256 newFixedUpperWad = newFixedLowerWad + 2 * int256(_sigmaWad);
-
-            // 3. Convert new fixed lower rate back to tick
-            int256 newTickLowerWad = -PRBMathSD59x18.div(PRBMathSD59x18.log2(int256(newFixedUpperWad)), 
-                                                        PRBMathSD59x18.log2(1000100000000000000)
-                                                        );
-
-            // 4. Convert new fixed upper rate back to tick
-            int256 newTickUpperWad = -PRBMathSD59x18.div(PRBMathSD59x18.log2(int256(newFixedLowerWad)),
-                                                        PRBMathSD59x18.log2(1000100000000000000)
-                                                        );
-
-            int256 newTickLower = newTickLowerWad/1e18;
-            int256 newTickUpper = newTickUpperWad/1e18;
-
-            newTickLowerMul = nearestTickMultiple(int24(newTickLower), _tickSpacing);
-            newTickUpperMul = nearestTickMultiple(int24(newTickUpper), _tickSpacing);
-
-            // Update the global upper and lower tick variables
-            _tickLower = newTickLowerMul;
-            _tickUpper = newTickUpperMul;
-
-            // Call to VoltzVault contract to update the position lower and upper ticks
-            // _vault.rebalance(IVoltzVault.TickRange(_tickLower, _tickUpper));
-
-            emit Rebalanced(newTickLowerMul, newTickUpperMul);
-            return (newTickLowerMul, newTickUpperMul);
+        } else {
+            // delta is less than or equal to 0 => choose 0
+            newFixedLowerWad = 0;
         }
+        // 2. Get the new tick upper
+        int256 newFixedUpperWad = newFixedLowerWad + 2 * int256(_sigmaWad);
+
+        // 3. Convert new fixed lower rate back to tick
+        int256 newTickLowerWad = -PRBMathSD59x18.div(
+            PRBMathSD59x18.log2(int256(newFixedUpperWad)),
+            PRBMathSD59x18.log2(1000100000000000000)
+        );
+
+        // 4. Convert new fixed upper rate back to tick
+        int256 newTickUpperWad = -PRBMathSD59x18.div(
+            PRBMathSD59x18.log2(int256(newFixedLowerWad)),
+            PRBMathSD59x18.log2(1000100000000000000)
+        );
+
+        int256 newTickLower = newTickLowerWad / 1e18;
+        int256 newTickUpper = newTickUpperWad / 1e18;
+
+        newTickLowerMul = nearestTickMultiple(int24(newTickLower), _tickSpacing);
+        newTickUpperMul = nearestTickMultiple(int24(newTickUpper), _tickSpacing);
+
+        // Update the global upper and lower tick variables
+        _tickLower = newTickLowerMul;
+        _tickUpper = newTickUpperMul;
+
+        // Call to VoltzVault contract to update the position lower and upper ticks
+        // _vault.rebalance(IVoltzVault.TickRange(_tickLower, _tickUpper));
+
+        emit Rebalanced(newTickLowerMul, newTickUpperMul);
+        return (newTickLowerMul, newTickUpperMul);
+    }
 }
