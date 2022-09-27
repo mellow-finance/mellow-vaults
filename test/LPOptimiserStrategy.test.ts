042e21489f5933ffe8ad2e387c711bae87215dba
Soleiman Anwary <soleiman.anwary@gmail.com>
2022-09-27 17:42:46 +0100

    feat: add new deploymentFixture for a separate vault implementation

--- test/LPOptimiserStrategy.test.ts
+++ test/LPOptimiserStrategy.test.ts
@@ -21,12 +21,223 @@ type DeployOptions = {};
 contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy", function () {
     this.timeout(200000);
 
-    const LOW_TICK = 0;
-    const HIGH_TICK = 6000;
+
 
     before(async () => {
-        this.deploymentFixture = deployments.createFixture(
+        this.deploymentFixtureOne = deployments.createFixture(
             async (_, __?: DeployOptions) => {
+                const LOW_TICK = 0;
+                const HIGH_TICK = 6000;
+
+                await deployments.fixture();
+                const { read } = deployments;
+
+                const { marginEngine, voltzPeriphery } = await getNamedAccounts();
+                this.marginEngine = marginEngine;
+                this.marginEngineContract = await ethers.getContractAt("IMarginEngine", this.marginEngine) as IMarginEngine;
+                this.vammContract = await ethers.getContractAt("IVAMM", await this.marginEngineContract.vamm()) as IVAMM;
+
+                await this.usdc.approve(
+                    this.marginEngine,
+                    ethers.constants.MaxUint256
+                );
+
+                await this.protocolGovernance
+                    .connect(this.admin)
+                    .stagePermissionGrants(this.usdc.address, [
+                        PermissionIdsLibrary.ERC20_VAULT_TOKEN,
+                    ]);
+                await sleep(await this.protocolGovernance.governanceDelay());
+                await this.protocolGovernance
+                    .connect(this.admin)
+                    .commitPermissionGrants(this.usdc.address);
+
+                const tokens = [this.usdc.address].map((t) => t.toLowerCase());
+
+                const startNft =
+                    (await read("VaultRegistry", "vaultsCount")).toNumber() + 1;
+
+                let voltzVaultNft = startNft;
+                let erc20VaultNft = startNft + 1;
+
+                await setupVault(
+                    hre,
+                    voltzVaultNft,
+                    "VoltzVaultGovernance",
+                    {
+                        createVaultArgs: [
+                            tokens,
+                            this.deployer.address,
+                            this.marginEngine,
+                            LOW_TICK,
+                            HIGH_TICK
+                        ],
+                    }
+                );
+
+                await setupVault(hre, erc20VaultNft, "ERC20VaultGovernance", {
+                    createVaultArgs: [tokens, this.deployer.address],
+                });
+
+                const { deploy } = deployments;
+
+                const erc20Vault = await read(
+                    "VaultRegistry",
+                    "vaultForNft",
+                    erc20VaultNft
+                );
+
+                const voltzVault = await read(
+                    "VaultRegistry",
+                    "vaultForNft",
+                    voltzVaultNft
+                );
+
+                this.erc20Vault = await ethers.getContractAt(
+                    "ERC20Vault",
+                    erc20Vault
+                );
+
+                this.voltzVault = await ethers.getContractAt(
+                    "VoltzVault",
+                    voltzVault
+                );
+
+                let strategyDeployParams = await deploy("LPOptimiserStrategy", {
+                    from: this.deployer.address,
+                    contract: "LPOptimiserStrategy",
+                    args: [
+                        this.erc20Vault.address,
+                        this.voltzVault.address,
+                        this.admin.address,
+                    ],
+                    log: true,
+                    autoMine: true,
+                });
+
+                await combineVaults(
+                    hre,
+                    erc20VaultNft + 1,
+                    [erc20VaultNft, voltzVaultNft],
+                    this.deployer.address,
+                    this.deployer.address
+                );
+
+                const erc20RootVault = await read(
+                    "VaultRegistry",
+                    "vaultForNft",
+                    erc20VaultNft + 1
+                );
+
+                this.erc20RootVault = await ethers.getContractAt(
+                    "ERC20RootVault",
+                    erc20RootVault
+                );
+
+                let usdcValidator = await deploy("ERC20Validator", {
+                    from: this.deployer.address,
+                    contract: "ERC20Validator",
+                    args: [this.protocolGovernance.address],
+                    log: true,
+                    autoMine: true,
+                });
+
+                await this.protocolGovernance
+                    .connect(this.admin)
+                    .stageValidator(
+                        this.usdc.address,
+                        usdcValidator.address
+                    );
+                await sleep(await this.protocolGovernance.governanceDelay());
+                await this.protocolGovernance
+                    .connect(this.admin)
+                    .commitValidator(this.usdc.address);
+
+                this.subject = await ethers.getContractAt(
+                    "LPOptimiserStrategy",
+                    strategyDeployParams.address
+                );
+
+                for (let address of [
+                    this.deployer.address,
+                    this.subject.address,
+                    this.erc20Vault.address,
+                ]) {
+                    await mint(
+                        "USDC",
+                        address,
+                        BigNumber.from(10).pow(6).mul(4000)
+                    );
+                    await this.usdc.approve(
+                        address,
+                        ethers.constants.MaxUint256
+                    );
+                }
+
+                await this.usdc.approve(
+                    this.marginEngine,
+                    ethers.constants.MaxUint256
+                );
+
+                await this.usdc.transfer(
+                    this.subject.address,
+                    BigNumber.from(10).pow(6).mul(3)
+                );
+
+                await this.voltzVaultGovernance
+                    .connect(this.admin)
+                    .stageDelayedProtocolParams({
+                        periphery: voltzPeriphery
+                    });
+                await sleep(86400);
+                await this.voltzVaultGovernance
+                    .connect(this.admin)
+                    .commitDelayedProtocolParams();
+
+                this.preparePush = async () => {
+
+                    await withSigner("0xb527e950fc7c4f581160768f48b3bfa66a7de1f0", async (s) => {
+                        await expect(
+                            this.marginEngineContract
+                                .connect(s)
+                                .setIsAlpha(false)
+                        ).to.not.be.reverted;
+
+                        await expect(
+                            this.vammContract
+                                .connect(s)
+                                .setIsAlpha(false)
+                        ).to.not.be.reverted;
+                    });
+                };
+
+                this.grantPermissionsVoltzVaults = async () => {
+                    let tokenId = await ethers.provider.send(
+                        "eth_getStorageAt",
+                        [
+                            this.voltzVault.address,
+                            "0x4", // address of _nft
+                        ]
+                    );
+                    await withSigner(
+                        this.erc20RootVault.address,
+                        async (erc20RootVaultSigner) => {
+                            await this.vaultRegistry
+                                .connect(erc20RootVaultSigner)
+                                .approve(this.subject.address, tokenId);
+                        }
+                    );
+                };
+
+                return this.subject;
+            }
+        )
+
+        this.deploymentFixtureTwo = deployments.createFixture(
+            async (_, __?: DeployOptions) => {
+                const LOW_TICK = -3000;
+                const HIGH_TICK = 0;
+
                 await deployments.fixture();
                 const { read } = deployments;
 
@@ -233,7 +444,7 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
     });
 
     beforeEach(async () => {
-        await this.deploymentFixture();
+        await this.deploymentFixtureOne();
 
         await this.subject.connect(this.admin).setLogProx(-1000);
         await this.subject.connect(this.admin).setSigmaWad(BigNumber.from("100000000000000000"));
@@ -241,21 +452,27 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
     });
 
     describe("Rebalance Logic", async () => {
-        // it( "Check if in-range position needs to be rebalanced", async () => {
-        //     // await this.subject.connect(this.admin).setTickValues(3000, 0, 6000);
-        //     const result = await this.subject.callStatic.rebalanceCheck();
-        //     expect(result).to.be.equal(false);
-        // })
+        it( "Check if in-range position needs to be rebalanced", async () => {
+            await this.deploymentFixtureTwo();
+            const result = await this.subject.callStatic.rebalanceCheck();
+            expect(result).to.be.equal(false);
+        })
         it("Check if out-of-range position needs to be rebalanced", async () => {
             const result = await this.subject.callStatic.rebalanceCheck();
             expect(result).to.be.equal(true);
         })
         it("Rebalance the position and return new ticks (max_poss_lower_bound < delta)", async () => {
             const currentFixedRateWad = BigNumber.from("2000000000000000000");
-            const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
-            expect(newTicks[0]).to.be.equal(-5220);
-            expect(newTicks[1]).to.be.equal(-4020);
+
+            if(await this.subject.callStatic.rebalanceCheck()) {
+                const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
+                expect(newTicks[0]).to.be.equal(-5220);
+                expect(newTicks[1]).to.be.equal(-4020);
+            } else {
+                throw new Error("Position does not need to be rebalanced");
+            }
         })
+
         it("Rebalance the position and return new ticks (max_poss_lower_bound > delta)", async () => {
             const currentFixedRateWad = BigNumber.from("1000000000000000000");
             const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
@@ -298,7 +515,7 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
             expect(newTicks[0]).to.be.equal(16020);
             expect(newTicks[1]).to.be.equal(69060);
         })
-        it("0 < deltaWad < 0.001", async () => { // exceeds the MAX_TICK value of 69100
+        it("0 < deltaWad < 0.001", async () => {
             const currentFixedRateWad = BigNumber.from("100010000000000000"); // 0.10001
             await this.subject.connect(this.admin).setSigmaWad(BigNumber.from("100000000000000000")); // 0.1
             const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
@@ -322,8 +539,7 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
     describe("Rebalance Event", async () => {
         it("Rebalance event was emitted after successful call on rebalance()", async () => {
             const currentFixedRateWad = BigNumber.from("2000100000000000000000");
-            // await this.subject.connect(this.admin).setTickValues(7000, 0, 6000);
-            const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
+            await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
             expect(
                 Object.entries(this.lPOptimiserStrategy.interface.events).some(
                     ([k, v]: any) => v.name === "RebalancedTicks"
@@ -332,8 +548,7 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
         })
         it("StrategyDeployment event was emitted after successful deployment of strategy", async () => {
             const currentFixedRateWad = BigNumber.from("2000100000000000000000");
-            // await this.subject.connect(this.admin).setTickValues(7000, 0, 6000);
-            const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
+            await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
             expect(
                 Object.entries(this.lPOptimiserStrategy.interface.events).some(
                     ([k, v]: any) => v.name === "StrategyDeployment"
@@ -346,6 +561,9 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
         it("_sigmaWad > currentFixedRateWad s.t. deltaWad < 0", async () => {
             const currentFixedRateWad = BigNumber.from("100000000000000000"); // 0.1
             await this.subject.connect(this.admin).setSigmaWad(BigNumber.from("200000000000000000")); // 0.2
+            const sigmaWad = await this.subject.getSigmaWad();
+            console.log("Print sigmaWad: ", sigmaWad.toString());
+
             const newTicks = await this.subject.connect(this.admin).callStatic.rebalanceTicks(currentFixedRateWad);
             expect(newTicks[0]).to.be.equal(9120);
             expect(newTicks[1]).to.be.equal(69060);
@@ -366,7 +584,7 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
                 expect(position.tickLower).to.be.equal(newTicks.newTickLowerMul);
                 expect(position.tickUpper).to.be.equal(newTicks.newTickUpperMul);
             } else {
-                console.log("Rebalance not needed");
+                throw new Error("Position does not need to be rebalanced");
             }
         })
     })
@@ -375,7 +593,8 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
         it("_sigmaWad = 0.05", async () => {
             const currentFixedRateWad = BigNumber.from("1500000000000000000");
             await this.subject.connect(this.admin).setSigmaWad(BigNumber.from("50000000000000000")); // 0.05
-            console.log("Print sigmaWad: ", await this.subject.getSigmaWad());
+            const sigmaWad = await this.subject.getSigmaWad();
+            console.log("Print sigmaWad: ", sigmaWad.toString());
 
             if (await this.subject.rebalanceCheck()) {
                 await this.subject.connect(this.admin).rebalanceTicks(currentFixedRateWad);
@@ -390,7 +609,7 @@ contract<LPOptimiserStrategy, DeployOptions, CustomContext>("LPOptimiserStrategy
                 console.log("f_l: ", newFixedLower, "f_u: ", newFixedUpper);
 
             } else {
-                console.log("Rebalance not needed");
+                throw new Error("Position does not need to be rebalanced");
             }
         })
     })
