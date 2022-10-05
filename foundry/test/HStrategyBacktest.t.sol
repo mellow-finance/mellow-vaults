d65d591aafe3e08a93bb03948a405df79e6eaf55
Dmitry Grigoryev <mitia810@mail.ru>
2022-10-05 16:48:07 +0300

    init

---
+++ foundry/test/HStrategyBacktest.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "../lib/forge-std/src/console2.sol";
+import "../lib/forge-std/src/Vm.sol";
+import "../lib/forge-std/src/Test.sol";
+
+contract HBacktest is Test {
+
+
+    function test() public {
+
+        
+    }
+}
