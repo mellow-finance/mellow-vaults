7505bee4f0c3f57af09110724913b9a5dd795d62
Andrey Borzenkov <armoking32@gmail.com>
2022-11-30 00:55:04 +0400

    Revert changes

--- deploy/0000_utils.ts
+++ deploy/0000_utils.ts
@@ -295,8 +295,8 @@ export const combineVaults = async (
         strategyPerformanceTreasuryAddress = strategyTreasuryAddress,
         tokenLimitPerAddress = ethers.constants.MaxUint256,
         tokenLimit = ethers.constants.MaxUint256,
-        managementFee = 0,
-        performanceFee = 0,
+        managementFee = 2 * 10 ** 7,
+        performanceFee = 2 * 10 ** 7,
     } = options || {};
 
     await setupVault(hre, expectedNft, "ERC20RootVaultGovernance", {
