af95d7085d2f3f91644fa64e9579d661856e5705
Dmitry Grigoryev <mitia810@mail.ru>
2023-04-12 15:34:12 +0300

    rebalance wrapper added

---
+++ contracts/utils/RebalanceWrapper.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.9;
+
+import "../libraries/ExceptionsLibrary.sol";
+import "../strategies/LStrategy.sol";
+import "./DefaultAccessControl.sol";
+
+contract RebalanceWrapper is DefaultAccessControl {
+
+    LStrategy public strategy;
+    int24 public maxTicksDelta;
+
+    constructor(address admin, address strategy_, int24 initialDelta) DefaultAccessControl(admin) {
+        strategy = LStrategy(strategy_);
+        maxTicksDelta = initialDelta;
+    }
+
+    function setDelta(int24 newMaxTicksDelta) external {
+        _requireAdmin();
+        maxTicksDelta = newMaxTicksDelta;
+    }
+
+    // -------------------  EXTERNAL, MUTATING  -------------------
+
+    function rebalanceUniV3Vaults(int24 offchainTick) external {
+        _requireAtLeastOperator();
+        IUniswapV3Pool pool = strategy.lowerVault().pool();
+        (, int24 spotTick, , , , , ) = pool.slot0();
+        require(offchainTick + maxTicksDelta >= spotTick && offchainTick - maxTicksDelta <= spotTick, ExceptionsLibrary.INVALID_STATE);
+        uint256[] memory minValues = new uint256[](2);
+
+        strategy.rebalanceUniV3Vaults(minValues, minValues, block.timestamp + 1);
+    }
+
+    function rebalanceERC20UniV3Vaults(int24 offchainTick) external {
+        _requireAtLeastOperator();
+        IUniswapV3Pool pool = strategy.lowerVault().pool();
+        (, int24 spotTick, , , , , ) = pool.slot0();
+        require(offchainTick + maxTicksDelta >= spotTick && offchainTick - maxTicksDelta <= spotTick, ExceptionsLibrary.INVALID_STATE);
+        uint256[] memory minValues = new uint256[](2);
+
+        strategy.rebalanceERC20UniV3Vaults(minValues, minValues, block.timestamp + 1);
+    }
+}
