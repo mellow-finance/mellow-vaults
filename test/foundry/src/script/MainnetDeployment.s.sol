e456fe35d8dc8a657f946a7c96c39e37eca9bb86
Dmitry Grigoryev <mitia810@mail.ru>
2023-04-18 01:25:43 +0300

    deployment

--- test/foundry/src/script/MainnetDeployment.s.sol
+++ test/foundry/src/script/MainnetDeployment.s.sol
@@ -11,47 +11,40 @@ import "../MockOracle.sol";
 import "../vaults/GearboxVault.sol";
 import "../vaults/GearboxRootVault.sol";
 import "../vaults/ERC20Vault.sol";
-import "../vaults/KyberVault.sol";
+import "../vaults/UniV3Vault.sol";
 
-import "../utils/KyberHelper.sol";
+import "../utils/UniV3Helper.sol";
 
-import "../vaults/KyberVaultGovernance.sol";
-import "../vaults/GearboxVaultGovernance.sol";
+import "../vaults/UniV3VaultGovernance.sol";
 import "../vaults/ERC20VaultGovernance.sol";
 import "../vaults/ERC20RootVaultGovernance.sol";
 
-import "../strategies/KyberPulseStrategyV2.sol";
-
-import "../interfaces/external/kyber/periphery/helpers/TicksFeeReader.sol";
-
+import "../strategies/PulseStrategyV2.sol";
 
 
 contract MainnetDeployment is Script {
 
     IERC20RootVault public rootVault;
     IERC20Vault erc20Vault;
-    IKyberVault kyberVault;
+    IUniV3Vault uniV3Vault;
 
-    KyberPulseStrategyV2 kyberStrategy;
+    PulseStrategyV2 strategy;
 
     uint256 nftStart;
-    address sAdmin = 0x36B16e173C5CDE5ef9f43944450a7227D71B4E31;
+    address sAdmin = 0x1EB0D48bF31caf9DBE1ad2E35b3755Fdf2898068;
     address protocolTreasury = 0x330CEcD19FC9460F7eA8385f9fa0fbbD673798A7;
     address strategyTreasury = 0x25C2B22477eD2E4099De5359d376a984385b4518;
     address deployer = 0x7ee9247b6199877F86703644c97784495549aC5E;
     address operator = 0x136348814f89fcbF1a0876Ca853D48299AFB8b3c;
 
-    address public bob = 0xB0B195aEFA3650A6908f15CdaC7D92F8a5791B0B;
-    address public stmatic = 0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4;
-    address public governance = 0x8Ff3148CE574B8e135130065B188960bA93799c6;
-    address public registry = 0xd3D0e85F225348a2006270Daf624D8c46cAe4E1F;
-    address public rootGovernance = 0xC12885af1d4eAfB8176905F16d23CD7A33D21f37;
-    address public erc20Governance = 0x05164eC2c3074A4E8eA20513Fbe98790FfE930A4;
-    address public kyberGovernance = 0x973C5550cE03E2009BF83513f6Ea362112C75602;
-    address public mellowOracle = 0x27AeBFEBDd0fde261Ec3E1DF395061C56EEC5836;
-
-    address public knc = 0x1C954E8fe737F99f68Fa1CCda3e51ebDB291948C;
-    address public usdc = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public wsteth = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address public governance = 0xDc9C17662133fB865E7bA3198B67c53a617B2153;
+    address public registry = 0xFD23F971696576331fCF96f80a20B4D3b31ca5b2;
+    address public rootGovernance = 0x973495e81180Cd6Ead654328A0bEbE01c8ad53EA;
+    address public erc20Governance = 0x0bf7B603389795E109a13140eCb07036a1534573;
+    address public uniV3Governance = 0x9c319DC47cA6c8c5e130d5aEF5B8a40Cce9e877e;
+    address public mellowOracle = 0x9d992650B30C6FB7a83E7e7a430b4e015433b838;
 
     IERC20RootVaultGovernance rootVaultGovernance = IERC20RootVaultGovernance(rootGovernance);
 
@@ -63,7 +56,7 @@ contract MainnetDeployment is Script {
             vaultRegistry.approve(address(rootVaultGovernance), nfts[i]);
         }
 
-        (IERC20RootVault w, uint256 nft) = rootVaultGovernance.createVault(tokens, address(kyberStrategy), nfts, deployer);
+        (IERC20RootVault w, uint256 nft) = rootVaultGovernance.createVault(tokens, address(strategy), nfts, deployer);
         rootVault = w;
         rootVaultGovernance.setStrategyParams(
             nft,
@@ -81,8 +74,8 @@ contract MainnetDeployment is Script {
                 managementFee: 0,
                 performanceFee: 0,
                 privateVault: false,
-                depositCallbackAddress: address(kyberStrategy),
-                withdrawCallbackAddress: address(kyberStrategy)
+                depositCallbackAddress: address(strategy),
+                withdrawCallbackAddress: address(strategy)
             })
         );
 
@@ -95,12 +88,8 @@ contract MainnetDeployment is Script {
         uint256 erc20VaultNft = vaultRegistry.vaultsCount() + 1;
 
         address[] memory tokens = new address[](2);
-        tokens[0] = stmatic;
-        tokens[1] = bob;
-
-        TicksFeesReader reader = new TicksFeesReader();
-
-        KyberHelper kyberHelper = new KyberHelper(IBasePositionManager(0x2B1c7b41f6A8F2b2bc45C3233a5d5FB3cD6dC9A8), reader);
+        tokens[0] = wsteth;
+        tokens[1] = weth;
 
         {
             IERC20VaultGovernance erc20VaultGovernance = IERC20VaultGovernance(erc20Governance);
@@ -109,35 +98,22 @@ contract MainnetDeployment is Script {
 
         IOracle oracle = IOracle(0x27AeBFEBDd0fde261Ec3E1DF395061C56EEC5836);
 
+        UniV3Helper helper = new UniV3Helper(INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88));
+
         {
-            IKyberVaultGovernance kyberVaultGovernance = IKyberVaultGovernance(kyberGovernance);
-/*
-            bytes[] memory P = new bytes[](1);
-            P[0] = abi.encodePacked(knc, uint24(1000), usdc, uint24(8), bob);
-
-            IKyberVaultGovernance.StrategyParams memory paramsC = IKyberVaultGovernance.StrategyParams({
-                farm: IKyberSwapElasticLM(0xBdEc4a045446F583dc564C0A227FFd475b329bf0),
-                paths: P,
-                pid: 117
-            });
-
-            vm.stopPrank();
-            vm.startPrank(deployer);
-*/
-            kyberVaultGovernance.createVault(tokens, deployer, 40);
- //           kyberVaultGovernance.setStrategyParams(erc20VaultNft + 1, paramsC);
+            IUniV3VaultGovernance uniGovernance = IUniV3VaultGovernance(uniV3Governance);
+            uniGovernance.createVault(tokens, deployer, 500, address(helper));
         }
 
         erc20Vault = IERC20Vault(vaultRegistry.vaultForNft(erc20VaultNft));
-        kyberVault = IKyberVault(vaultRegistry.vaultForNft(erc20VaultNft + 1));
+        uniV3Vault = IUniV3Vault(vaultRegistry.vaultForNft(erc20VaultNft + 1));
 
-        kyberStrategy = new KyberPulseStrategyV2(IBasePositionManager(0x2B1c7b41f6A8F2b2bc45C3233a5d5FB3cD6dC9A8));
+        strategy = new PulseStrategyV2(INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88));
 
-        KyberPulseStrategyV2.ImmutableParams memory sParams = KyberPulseStrategyV2.ImmutableParams({
-            router: 0x1111111254EEB25477B68fb85Ed929f73A960582,
+        PulseStrategyV2.ImmutableParams memory sParams = PulseStrategyV2.ImmutableParams({
             erc20Vault: erc20Vault,
-            kyberVault: kyberVault,
-            mellowOracle: oracle,
+            uniV3Vault: uniV3Vault,
+            router: 0x1111111254EEB25477B68fb85Ed929f73A960582,
             tokens: tokens
         });
 
@@ -145,10 +121,10 @@ contract MainnetDeployment is Script {
         AA[0] = 10**15;
         AA[1] = 10**15;
 
-        KyberPulseStrategyV2.MutableParams memory smParams = KyberPulseStrategyV2.MutableParams({
+        PulseStrategyV2.MutableParams memory smParams = PulseStrategyV2.MutableParams({
             priceImpactD6: 0,
-            defaultIntervalWidth: 4000,
-            maxPositionLengthInTicks: 10000,
+            defaultIntervalWidth: 280,
+            maxPositionLengthInTicks: 700,
             maxDeviationForVaultPool: 50,
             timespanForAverageTick: 60,
             neighborhoodFactorD: 10 ** 7 * 15,
@@ -158,10 +134,6 @@ contract MainnetDeployment is Script {
             minSwapAmounts: AA
         });
 
-     //   kyberVault.updateFarmInfo();
-
-     //   preparePush(address(kyberVault));
-
         {
             uint256[] memory nfts = new uint256[](2);
             nfts[0] = erc20VaultNft;
@@ -169,18 +141,49 @@ contract MainnetDeployment is Script {
             combineVaults(tokens, nfts);
         }
 
-        kyberStrategy.initialize(sParams, deployer);
-        kyberStrategy.updateMutableParams(smParams);
+        strategy.initialize(sParams, deployer);
+        strategy.updateMutableParams(smParams);
 
-        console2.log("strategy:", address(kyberStrategy));
+        IVaultRegistry(registry).transferFrom(deployer, sAdmin, erc20VaultNft + 2);
+
+        bytes32 ADMIN_ROLE =
+        bytes32(0xf23ec0bb4210edd5cba85afd05127efcd2fc6a781bfed49188da1081670b22d8); // keccak256("admin)
+        bytes32 ADMIN_DELEGATE_ROLE =
+            bytes32(0xc171260023d22a25a00a2789664c9334017843b831138c8ef03cc8897e5873d7); // keccak256("admin_delegate")
+        bytes32 OPERATOR_ROLE =
+            bytes32(0x46a52cf33029de9f84853745a87af28464c80bf0346df1b32e205fc73319f622); // keccak256("operator")
+
+        strategy.grantRole(ADMIN_ROLE, sAdmin);
+        strategy.grantRole(ADMIN_DELEGATE_ROLE, sAdmin);
+        strategy.grantRole(ADMIN_DELEGATE_ROLE, deployer);
+        strategy.grantRole(OPERATOR_ROLE, sAdmin);
+        strategy.revokeRole(ADMIN_DELEGATE_ROLE, deployer);
+        strategy.revokeRole(ADMIN_ROLE, deployer);
+
+        console2.log("strategy:", address(strategy));
         console2.log("root vault:", address(rootVault));
         console2.log("erc20 vault:", address(erc20Vault));
-        console2.log("kyber vault:", address(kyberVault));
+        console2.log("uni vault:", address(uniV3Vault));
     }
 
     function run() external {
         vm.startBroadcast();
 
-        kek();
+        IERC20(weth).transfer(0xFDF8B88D77a9B65646e0D9Cd5880E3677B94Af01, 10**12);
+        IERC20(wsteth).transfer(0xFDF8B88D77a9B65646e0D9Cd5880E3677B94Af01, 10**12);
+
+        rootVault = IERC20RootVault(0x5Fd7eA4e9F96BBBab73D934618a75746Fd88e460);
+
+      //  IERC20(weth).approve(address(rootVault), 10**20);
+      //  IERC20(wsteth).approve(address(rootVault), 10**20);
+
+        uint256[] memory A = new uint256[](2);
+        A[0] = 4 * 10**16;
+        A[1] = 4 * 10**16;
+
+        //rootVault.deposit(A, 0, "");
+
+
+     //   kek();
     }
 }
