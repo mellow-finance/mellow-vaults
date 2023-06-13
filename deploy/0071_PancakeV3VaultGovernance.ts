4fe8a8b300bacc01f5910743fa5348394a01c648
Andrey Borzenkov <armoking32@gmail.com>
2023-06-13 18:58:02 +0400

    deploy script

---
+++ deploy/0071_PancakeV3VaultGovernance.ts
@@ -0,0 +1,53 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
+import { MAIN_NETWORKS, TRANSACTION_GAS_LIMITS } from "./0000_utils";
+import { BigNumber } from "ethers";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const { deployments, getNamedAccounts } = hre;
+    const { deploy, get } = deployments;
+    const protocolGovernance = await get("ProtocolGovernance");
+    const vaultRegistry = await get("VaultRegistry");
+    const { deployer, pancakePositionManager } = await getNamedAccounts();
+
+    const { address: mellowOracle } = await get("MellowOracle");
+    const { address: singleton } = await deploy("PancakeSwapVault", {
+        from: deployer,
+        args: [],
+        log: true,
+        autoMine: true,
+        ...TRANSACTION_GAS_LIMITS,
+        gasLimit: BigNumber.from(10 ** 6 * 6)
+    });
+    
+    await deploy("PancakeSwapVaultGovernance", {
+        from: deployer,
+        args: [
+            {
+                protocolGovernance: protocolGovernance.address,
+                registry: vaultRegistry.address,
+                singleton,
+            },
+            {
+                positionManager: pancakePositionManager,
+                oracle: mellowOracle,
+            },
+        ],
+        log: true,
+        autoMine: true,
+        ...TRANSACTION_GAS_LIMITS,
+        gasLimit: BigNumber.from(10 ** 6 * 3)
+    });
+};
+export default func;
+func.tags = [
+    "PancakeSwapVaultGovernance",
+    "core",
+    ...MAIN_NETWORKS,
+    "arbitrum",
+    "optimism",
+    "polygon",
+];
+func.dependencies = ["ProtocolGovernance", "VaultRegistry"];
